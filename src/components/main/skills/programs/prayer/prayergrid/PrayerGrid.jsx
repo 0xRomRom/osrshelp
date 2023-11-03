e5a798eac8ef18ef397bfca16407185fc498af88
Roman - <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>
2023-11-03 23:35:16 +0100

    Got all prices and exp per bone

--- src/components/main/skills/programs/prayer/prayergrid/PrayerGrid.jsx
+++ src/components/main/skills/programs/prayer/prayergrid/PrayerGrid.jsx
@@ -19,7 +19,19 @@ const PrayerGrid = (props) => {
       "https://api.weirdgloop.org/exchange/history/osrs/latest?name=Bones|Wolf%20bones|Burnt%20bones|Monkey%20bones|Bat%20bones|Big%20bones|Jogre%20bones|Zogre%20bones|Shaikahan%20bones|Babydragon%20bones|Wyrm%20bones|Wyvern%20bones|Dragon%20bones|Drake%20bones|Fayrg%20bones|Lava%20dragon%20bones|Raurg%20bones|Hydra%20bones|Dagannoth%20bones|Ourg%20bones|Superior%20dragon%20bones|Ensouled%20goblin%20head|Ensouled%20monkey%20head|Ensouled%20imp%20head|Ensouled%20minotaur%20head|Ensouled%20scorpion%20head|Ensouled%20bear%20head|Ensouled%20unicorn%20head|Ensouled%20dog%20head|Ensouled%20chaos%20druid%20head|Ensouled%20giant%20head|Ensouled%20ogre%20head|Ensouled%20elf%20head|Ensouled%20troll%20head|Ensouled%20horror%20head|Ensouled%20kalphite%20head|Ensouled%20dagannoth%20head|Ensouled%20bloodveld%20head|Ensouled%20tzhaar%20head|Ensouled%20demon%20head|Ensouled%20hellhound%20head|Ensouled%20aviansie%20head|Ensouled%20abyssal%20head|Ensouled%20dragon%20head"
     );
     const result = await fetcher.json();
-    console.log(result);
+    // console.log(result);
+    let bonesList = JSON.parse(JSON.stringify(prayerList));
+    // console.log(bonesList);
+
+    for (let i = 0; i < bonesList.length; i++) {
+      const boneId = bonesList[i].name;
+      if (result.hasOwnProperty(boneId)) {
+        bonesList[i].cost = result[boneId].price;
+      }
+    }
+
+    console.log("Boneslist After:", bonesList);
+
     setBonesDB(result);
   }, [bonesDB]);
 
