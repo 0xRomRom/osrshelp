21fd974c5c3e9cee194354495489f033141cc431
Roman - <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>
2023-11-06 14:31:57 +0100

    Total bone cost displayed

--- src/components/main/skills/programs/prayer/prayergrid/PrayerGrid.jsx
+++ src/components/main/skills/programs/prayer/prayergrid/PrayerGrid.jsx
@@ -9,6 +9,7 @@ import { useEffect, useState, useCallback } from "react";
 
 const PrayerGrid = (props) => {
   const [initialDB, setInitialDB] = useState([]);
+  const [initialFetch, setInitialFetch] = useState(false);
   const [bonesDB, setBonesDB] = useState(prayerList);
   const [xpSorted, setXPSorted] = useState(false);
   //   const [memberSorted, setMemberSorted] = useState(false);
@@ -16,25 +17,30 @@ const PrayerGrid = (props) => {
   const [costSorted, setCostSorted] = useState(false);
 
   useEffect(() => {
-    let result = [];
-
     const fetchData = async () => {
-      if (!initialDB) {
+      let result = [];
+      if (!initialFetch) {
+        setInitialFetch(!initialFetch);
         const fetcher = await fetch(
           "https://api.weirdgloop.org/exchange/history/osrs/latest?name=Bones|Wolf%20bones|Burnt%20bones|Monkey%20bones|Bat%20bones|Big%20bones|Jogre%20bones|Zogre%20bones|Shaikahan%20bones|Babydragon%20bones|Wyrm%20bones|Wyvern%20bones|Dragon%20bones|Drake%20bones|Fayrg%20bones|Lava%20dragon%20bones|Raurg%20bones|Hydra%20bones|Dagannoth%20bones|Ourg%20bones|Superior%20dragon%20bones|Ensouled%20goblin%20head|Ensouled%20monkey%20head|Ensouled%20imp%20head|Ensouled%20minotaur%20head|Ensouled%20scorpion%20head|Ensouled%20bear%20head|Ensouled%20unicorn%20head|Ensouled%20dog%20head|Ensouled%20chaos%20druid%20head|Ensouled%20giant%20head|Ensouled%20ogre%20head|Ensouled%20elf%20head|Ensouled%20troll%20head|Ensouled%20horror%20head|Ensouled%20kalphite%20head|Ensouled%20dagannoth%20head|Ensouled%20bloodveld%20head|Ensouled%20tzhaar%20head|Ensouled%20demon%20head|Ensouled%20hellhound%20head|Ensouled%20aviansie%20head|Ensouled%20abyssal%20head|Ensouled%20dragon%20head"
         );
         result = await fetcher.json();
+        setInitialDB(result);
       }
-      setInitialDB(result);
-      let bonesList = prayerList;
-
+      if (initialFetch) {
+        result = initialDB;
+      }
+      let bonesList = JSON.parse(JSON.stringify(prayerList));
+      console.log(result);
+      //Set initial boneprice
       for (let i = 0; i < bonesList.length; i++) {
         const boneId = bonesList[i].name;
         if (result.hasOwnProperty(boneId)) {
-          bonesList[i].cost = +result[boneId].price;
+          bonesList[i].price = +result[boneId].price;
         }
       }
 
+      //Calc bones to use
       let dbCopy = JSON.parse(JSON.stringify(bonesList));
       const expToGo = props.remainingExp;
 
@@ -43,11 +49,9 @@ const PrayerGrid = (props) => {
         dbCopy[i].toGo = Math.ceil(expToGo / boneExp);
       }
       setBonesDB(dbCopy);
-
-      //   setBonesDB(bonesList);
     };
     fetchData();
-  }, [bonesDB.length, props.remainingExp, initialDB]);
+  }, [bonesDB.length, props.remainingExp, initialDB, initialFetch]);
 
   //   useEffect(() => {
   //     const calculateBonesToUse = () => {
@@ -121,12 +125,12 @@ const PrayerGrid = (props) => {
 
   const sortCost = () => {
     if (costSorted) {
-      const sorter = bonesDB.sort((a, b) => a.cost - b.cost);
+      const sorter = bonesDB.sort((a, b) => a.price - b.price);
       setBonesDB(sorter);
       setCostSorted(!costSorted);
       return;
     }
-    const sorter = bonesDB.sort((a, b) => b.cost - a.cost);
+    const sorter = bonesDB.sort((a, b) => b.price - a.price);
     setBonesDB(sorter);
     setCostSorted(!costSorted);
   };
@@ -163,7 +167,9 @@ const PrayerGrid = (props) => {
               </span>
               <span className={stl.rowItem}>{bone.exp}</span>
               <span className={stl.rowItem}> {bone.toGo.toLocaleString()}</span>
-              <span className={stl.rowItem}>{bone.cost.toLocaleString()}</span>
+              <span className={stl.rowItem}>
+                {(bone.toGo * bone.price).toLocaleString()}
+              </span>
             </div>
           );
         })}
