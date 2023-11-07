6f249770ad7e185544b37ed422e110ac551ef51b
Roman - <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>
2023-11-07 01:17:18 +0100

    Checkboxes with values created

--- src/components/main/skills/programs/prayer/PrayerFilters.jsx
+++ src/components/main/skills/programs/prayer/PrayerFilters.jsx
@@ -1,17 +1,105 @@
 import stl from "./PrayerFilters.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useState, useEffect } from "react";
 
 const PrayerFilters = (props) => {
-  const handleMonsterSearch = (e) => {
-    props.setSearchState(e.target.value);
+  const [checkboxState, setCheckboxState] = useState({});
+  const [multiplier, setMultiplier] = useState(0);
+
+  const handleCheckboxChange = (e) => {
+    const checkboxId = e.target.id;
+    const isChecked = e.target.checked;
+
+    const chosenMultiplier = +e.target.dataset.multiplier;
+
+    checkboxState[checkboxId] === true
+      ? setMultiplier(0)
+      : setMultiplier(chosenMultiplier);
+
+    setCheckboxState({ [checkboxId]: isChecked });
+
+    // Uncheck other checkboxes
+    for (const id in checkboxState) {
+      if (id !== checkboxId) {
+        setCheckboxState((prevState) => ({
+          ...prevState,
+          [id]: false,
+        }));
+      }
+    }
   };
+
+  useEffect(() => {
+    console.log(multiplier);
+  }, [multiplier]);
 
   return (
     <div className={stl.modal}>
-      <div className={stl.checkRow}>
-        <input type="checkbox" id="gilded-altar" />
-        <label htmlFor="gilded-altar">Lit Gilded Altar (+350%)</label>
+      <div className={stl.innerWrap}>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="gilded-altar"
+            data-multiplier="350"
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="gilded-altar">Lit Gilded Altar (+350%)</label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="ecto-funtus"
+            data-multiplier="400"
+            onChange={handleCheckboxChange}
+          />
+          <label htmlFor="ecto-funtus">Ectofuntus (+400%)</label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="chaos-altar"
+            onChange={handleCheckboxChange}
+            data-multiplier="700"
+          />
+          <label htmlFor="chaos-altar">Chaos Altar (+700%)</label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="sacred-bone"
+            onChange={handleCheckboxChange}
+            data-multiplier="300"
+          />
+          <label htmlFor="sacred-bone">Sacred Bone Burner (+300%)</label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="dark-altar"
+            onChange={handleCheckboxChange}
+            data-multiplier="400"
+          />
+          <label htmlFor="dark-altar">Relic: Dark Altar Devotion (+400%)</label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="xeric-altar"
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="xeric-altar">Relic: Xeric's Wisdom (+200%)</label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="demonic-offering"
+            onChange={handleCheckboxChange}
+            data-multiplier="300"
+          />
+          <label htmlFor="demonic-offering">Demonic Offering (+300%)</label>
+        </div>
       </div>
     </div>
   );
