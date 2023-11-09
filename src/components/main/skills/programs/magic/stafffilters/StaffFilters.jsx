160fde1b84652c55d627907ac4b244d7b7f38468
Roman - <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>
2023-11-09 07:00:59 +0100

    All staves added

--- src/components/main/skills/programs/magic/stafffilters/StaffFilters.jsx
+++ src/components/main/skills/programs/magic/stafffilters/StaffFilters.jsx
@@ -53,6 +53,159 @@ const StaffFilters = ({
             Staff of air
           </label>
         </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="staffofwater"
+            checked={checkboxState["staffofwater"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="staffofwater">
+            <img
+              src="/staves/Staff_of_water.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Staff of water
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="staffofearth"
+            checked={checkboxState["staffofearth"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="staffofearth">
+            <img
+              src="/staves/Staff_of_earth.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Staff of earth
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="staffoffire"
+            checked={checkboxState["staffoffire"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="staffoffire">
+            <img
+              src="/staves/Staff_of_fire.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Staff of fire
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="lavabattlestaff"
+            checked={checkboxState["lavabattlestaff"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="lavabattlestaff">
+            <img
+              src="/staves/Lava_battlestaff.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Lava battlestaff
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="mudbattlestaff"
+            checked={checkboxState["mudbattlestaff"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="mudbattlestaff">
+            <img
+              src="/staves/Mud_battlestaff.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Mud battlestaff
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="steambattlestaff"
+            checked={checkboxState["steambattlestaff"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="steambattlestaff">
+            <img
+              src="/staves/Steam_battlestaff.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Steam battlestaff
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="smokebattlestaff"
+            checked={checkboxState["smokebattlestaff"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="smokebattlestaff">
+            <img
+              src="/staves/Smoke_battlestaff.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Smoke battlestaff
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="mistbattlestaff"
+            checked={checkboxState["mistbattlestaff"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="mistbattlestaff">
+            <img
+              src="/staves/Mist_battlestaff.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Mist battlestaff
+          </label>
+        </div>
+        <div className={stl.checkRow}>
+          <input
+            type="checkbox"
+            id="dustbattlestaff"
+            checked={checkboxState["dustbattlestaff"] || false}
+            onChange={handleCheckboxChange}
+            data-multiplier="200"
+          />
+          <label htmlFor="dustbattlestaff">
+            <img
+              src="/staves/Dust_battlestaff.webp"
+              alt="Staff of air"
+              className={stl.staffImg}
+            />
+            Dust battlestaff
+          </label>
+        </div>
       </div>
     </div>
   );
