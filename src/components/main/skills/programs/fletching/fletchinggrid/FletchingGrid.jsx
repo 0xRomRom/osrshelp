2742e84e3b87274f2a6a1b977e945fe73aa3036a
Roman - <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>
2023-11-11 05:31:01 +0100

    Filters working

--- src/components/main/skills/programs/fletching/fletchinggrid/FletchingGrid.jsx
+++ src/components/main/skills/programs/fletching/fletchinggrid/FletchingGrid.jsx
@@ -1,23 +1,22 @@
 import stl from "./FletchingGrid.module.css";
 import FLETCHLIST from "../../../../../../utils/fletchlist";
 import fletchingLogo from "../../../../../../assets/skillicons/Fletching.webp";
-import axeLogo from "../../../../../../assets/random/Rune_axe.png";
 import donate from "../../../../../../assets/icons/Donate.webp";
 import statsLogo from "../../../../../../assets/random/Stats_icon.webp";
 
 import { useState, useCallback, useEffect } from "react";
 
 const FletchingGrid = (props) => {
-  const [treeDB, setTreeDB] = useState(FLETCHLIST);
+  const [fletchDB, setFletchDB] = useState(FLETCHLIST);
   const [monsterSorted, setMonsterSorted] = useState(false);
   const [memberSorted, setMemberSorted] = useState(false);
   const [combatSorted, setCombatSorted] = useState(false);
   const [toGoSorted, setToGoSorted] = useState(false);
 
   const calculateFletchActions = useCallback(
-    (tree) => {
+    (fletch) => {
       const expToGo = props.remainingExp;
-      const result = Math.ceil(expToGo / tree.exp);
+      const result = Math.ceil(expToGo / fletch.exp);
       return result ? result : "?";
     },
     [props.remainingExp]
@@ -27,50 +26,50 @@ const FletchingGrid = (props) => {
     console.log(props.multiplier);
   }, [props.multiplier]);
 
-  const sortTree = () => {
+  const sortItem = () => {
     setMonsterSorted(!monsterSorted);
-    let sorter = [...treeDB];
+    let sorter = [...fletchDB];
     console.log(sorter);
     sorter.sort((a, b) =>
       monsterSorted ? a.level - b.level : b.level - a.level
     );
-    setTreeDB(sorter);
+    setFletchDB(sorter);
   };
 
-  const sortMembers = () => {
+  const sortAmount = () => {
     setMemberSorted(!memberSorted);
-    let sorter = [...treeDB];
+    let sorter = [...fletchDB];
     sorter.sort((a, b) =>
-      memberSorted ? a.members - b.members : b.members - a.members
+      memberSorted ? a.exp - b.exp - b.exp : b.exp - a.exp
     );
-    setTreeDB(sorter);
+    setFletchDB(sorter);
   };
 
   const sortExp = () => {
     setCombatSorted(!combatSorted);
-    let sorter = [...treeDB];
+    let sorter = [...fletchDB];
     sorter.sort((a, b) => (combatSorted ? a.exp - b.exp : b.exp - a.exp));
-    setTreeDB(sorter);
+    setFletchDB(sorter);
   };
 
   const sortToGo = () => {
     setToGoSorted(!toGoSorted);
-    let sorter = [...treeDB];
+    let sorter = [...fletchDB];
     sorter.sort((a, b) => (toGoSorted ? a.exp - b.exp : b.exp - a.exp));
-    setTreeDB(sorter);
+    setFletchDB(sorter);
   };
 
   return (
     <div className={stl.grid}>
       <div className={stl.typeRow}>
-        <span className={stl.monsterTitleRow} onClick={sortTree}>
+        <span className={stl.monsterTitleRow} onClick={sortItem}>
           <img src={fletchingLogo} alt="Tree Logo" className={stl.miniLogo} />{" "}
           Item
         </span>
         <span onClick={sortExp}>
           <img src={statsLogo} alt="Health Logo" className={stl.miniLogo} /> Exp
         </span>
-        <span onClick={sortMembers}>
+        <span onClick={sortAmount}>
           <img
             src="/fletching/Knife.webp"
             alt="Member Logo"
@@ -83,24 +82,24 @@ const FletchingGrid = (props) => {
         </span>
       </div>
       <div className={stl.resultGrid}>
-        {treeDB.map((tree) => {
-          const treePrice = calculateFletchActions(tree);
+        {fletchDB.map((fletch) => {
+          const fletchAmount = calculateFletchActions(fletch).toLocaleString();
           return (
             <div className={stl.row} key={Math.random()}>
               <span className={`${stl.rowItem} ${stl.monsterRow}`}>
                 <span className={stl.innerSpan}>
                   <img
-                    src={tree.src}
+                    src={fletch.src}
                     alt="Oldschool Runescape Trees"
                     className={stl.minifood}
                   />
-                  <span className={stl.lvlSpan}>Lvl {tree.level}</span>
-                  {tree.name}
+                  <span className={stl.lvlSpan}>Lvl {fletch.level}</span>
+                  {fletch.name}
                 </span>
               </span>
-              <span className={stl.rowItem}>{tree.exp}</span>
-              <span className={stl.rowItem}>{tree.members ? "Yes" : "No"}</span>
-              <span className={stl.rowItem}>{treePrice.toLocaleString()}</span>
+              <span className={stl.rowItem}>{fletch.exp}</span>
+              <span className={stl.rowItem}>{fletchAmount}</span>
+              <span className={stl.rowItem}>{fletch.cost}</span>
             </div>
           );
         })}
