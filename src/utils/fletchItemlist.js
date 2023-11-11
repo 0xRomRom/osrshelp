d06930facd74a2403a0e02ee66cda056c059f0b9
Roman - <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>
2023-11-11 06:36:13 +0100

    Item skeleton object created

--- src/utils/fletchItemlist.js
+++ src/utils/fletchItemlist.js
@@ -1,3 +1,352 @@
-const FLETCHITEMLIST = [];
+const FLETCHITEMLIST = [
+  {
+    // Headless arrow
+  },
+  {
+    // Bronze arrow
+  },
+  {
+    // Bronze javelin
+  },
+  {
+    // Ogre arrow
+  },
+  {
+    // Shortbow (u)
+  },
+  {
+    // Shortbow
+  },
+  {
+    // Wooden stock
+  },
+  {
+    // Bronze crossbow (u)
+  },
+  {
+    // Bronze crossbow
+  },
+  {
+    // Bronze bolts
+  },
+  {
+    // Bronze dart
+  },
+  {
+    // Longbow (u)
+  },
+  {
+    // Longbow
+  },
+  {
+    // Opal-tipped bronze bolts
+  },
+  {
+    // Iron arrow
+  },
+  {
+    // Iron javelin
+  },
+  {
+    // Oak shortbow (u)
+  },
+  {
+    // Oak shortbow
+  },
+  {
+    // Iron dart
+  },
+  {
+    // Blurite crossbow (u)
+  },
+  {
+    // Blurite crossbow
+  },
+  {
+    // Blurite bolts
+  },
+  {
+    // Oak longbow (u)
+  },
+  {
+    // Oak longbow
+  },
+  {
+    // Jade-tipped blurite bolts
+  },
+  {
+    // Oak shield
+  },
+  {
+    // Steel arrow
+  },
+  {
+    // Steel javelin
+  },
+  {
+    // Kebbit bolts
+  },
+  {
+    // Willow shortbow (u)
+  },
+  {
+    // Willow shortbow
+  },
+  {
+    // Steel dart
+  },
+  {
+    // Willow stock
+  },
+  {
+    // Iron crossbow (u)
+  },
+  {
+    // Iron crossbow
+  },
+  {
+    // Iron bolts
+  },
+  {
+    // Willow longbow (u)
+  },
+  {
+    // Willow longbow
+  },
+  {
+    // Battlestaves
+  },
+  {
+    // Pearl-tipped iron bolts
+  },
+  {
+    // Willow shield
+  },
+  {
+    // Long kebbit bolts
+  },
+  {
+    // Silver bolts
+  },
+  {
+    // Mithril arrow
+  },
+  {
+    // Teak stock
+  },
+  {
+    // Steel crossbow (u)
+  },
+  {
+    // Steel crossbow
+  },
+  {
+    // Steel bolts
+  },
+  {
+    // Mithril javelin
+  },
+  {
+    // Red topaz-tipped steel bolts
+  },
+  {
+    // Maple shortbow (u)
+  },
+  {
+    // Maple shortbow
+  },
+  {
+    // Barbed-tipped bronze bolts
+  },
+  {
+    // Mithril dart
+  },
+  {
+    // Broad arrows
+  },
+  {
+    // Toxic blowpipe
+  },
+  {
+    // Maple stock
+  },
+  {
+    // Mithril bolts
+  },
+  {
+    // Mithril crossbow (u)
+  },
+  {
+    // Mithril crossbow
+  },
+  {
+    // Broad bolts
+  },
+  {
+    // Maple longbow (u)
+  },
+  {
+    // Maple longbow
+  },
+  {
+    // Sapphire-tipped mithril bolts
+  },
+  {
+    // Maple shield
+  },
+  {
+    // Emerald-tipped mithril bolts
+  },
+  {
+    // Adamant arrow
+  },
+  {
+    // Adamant bolts
+  },
+  {
+    // Mahogany stock
+  },
+  {
+    // Adamant crossbow (u)
+  },
+  {
+    // Adamant crossbow
+  },
+  {
+    // Adamant javelin
+  },
+  {
+    // Ruby-tipped adamant bolts
+  },
+  {
+    // Diamond-tipped adamant bolts
+  },
+  {
+    // Yew shortbow (u)
+  },
+  {
+    // Yew shortbow
+  },
+  {
+    // Adamant dart
+  },
+  {
+    // Runite bolts
+  },
+  {
+    // Yew stock
+  },
+  {
+    // Runite crossbow (u)
+  },
+  {
+    // Runite crossbow
+  },
+  {
+    // Yew longbow (u)
+  },
+  {
+    // Yew longbow
+  },
+  {
+    // Dragonstone-tipped runite bolts
+  },
+  {
+    // Yew shield
+  },
+  {
+    // Onyx-tipped runite bolts
+  },
+  {
+    // Rune arrow
+  },
+  {
+    // Amethyst-tipped broad bolts
+  },
+  {
+    // Rune javelin
+  },
+  {
+    // Magic stock
+  },
+  {
+    // Dragon crossbow (u)
+  },
+  {
+    // Dragon crossbow
+  },
+  {
+    // Magic shortbow (u)
+  },
+  {
+    // Magic shortbow
+  },
+  {
+    // Rune dart
+  },
+  {
+    // Amethyst arrow
+  },
+  {
+    // Dragon bolts
+  },
+  {
+    // Opal dragon bolts
+  },
+  {
+    // Jade dragon bolts
+  },
+  {
+    // Pearl dragon bolts
+  },
+  {
+    // Topaz dragon bolts
+  },
+  {
+    // Sapphire dragon bolts
+  },
+  {
+    // Emerald dragon bolts
+  },
+  {
+    // Ruby dragon bolts
+  },
+  {
+    // Diamond dragon bolts
+  },
+  {
+    // Dragonstone dragon bolts
+  },
+  {
+    // Onyx dragon bolts
+  },
+  {
+    // Amethyst javelin
+  },
+  {
+    // Magic longbow (u)
+  },
+  {
+    // Magic longbow
+  },
+  {
+    // Magic shield
+  },
+  {
+    // Amethyst dart
+  },
+  {
+    // Dragon arrow
+  },
+  {
+    // Redwood shields
+  },
+  {
+    // Dragon javelin
+  },
+  {
+    // Dragon dart
+  },
+];
 
 export default FLETCHITEMLIST;
