054ba3eec234a508affdaa6d17c9a7327a00a2ec
Roman - <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>
2023-12-01 17:41:38 +0100

    Dynamic number counter created

--- src/components/main/home/totalUsersBox/TotalUsers.jsx
+++ src/components/main/home/totalUsersBox/TotalUsers.jsx
@@ -1,10 +1,20 @@
 import stl from "./Totalusers.module.css";
+import { useState } from "react";
+import { useInView } from "react-intersection-observer";
+import NumberCounter from "../../../../utils/NumberCounter";
 
 const TotalUsers = () => {
+  const [totalUsers, setTotalUsers] = useState(3341);
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+  });
   return (
     <div className={stl.totalusers}>
-      <h2 className={stl.userCount}>3391</h2>
-      <span className={stl.visitorsSpan}>Total visitors</span>
+      <h2 className={stl.userCount} ref={ref}>
+        {" "}
+        {inView && <NumberCounter n={totalUsers} />}
+      </h2>
+      <span className={stl.visitorsSpan}>Total users</span>
     </div>
   );
 };
