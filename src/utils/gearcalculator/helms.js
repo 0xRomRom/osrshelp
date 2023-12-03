c903d24cb7b9124e847f4a8a924ff17e075800bb
0xRomRom <roman0811@live.nl>
2023-12-03 06:44:51 +0100

    Helmets until black added

--- src/utils/gearcalculator/helms.js
+++ src/utils/gearcalculator/helms.js
@@ -10,45 +10,307 @@ const HELMS = [
     rangedLvl: 0,
     magicLvl: 0,
     strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -3,
+        Range: 0,
+      },
+      // Defence
+      {
+        Stab: 3,
+        Slash: 4,
+        Crush: 2,
+        Magic: -1,
+        Range: 3,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 1.814,
+      },
+    ],
     src: "./gearcalculator/helms/Bronze_med_helm.webp",
   },
   {
     type: "Melee",
-    name: "Bronze Full Helm",
+    name: "Bronze full helm",
     defLvl: 1,
     rangedLvl: 0,
     magicLvl: 0,
     strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -6,
+        Range: -3,
+      },
+      // Defence
+      {
+        Stab: 4,
+        Slash: 5,
+        Crush: 3,
+        Magic: -1,
+        Range: 4,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 2.721,
+      },
+    ],
     src: "./gearcalculator/helms/Bronze_full_helm.webp",
+  },
+  {
+    type: "Melee",
+    name: "Iron med helm",
+    defLvl: 1,
+    rangedLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -3,
+        Range: 0,
+      },
+      // Defence
+      {
+        Stab: 4,
+        Slash: 5,
+        Crush: 3,
+        Magic: -1,
+        Range: 4,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 1.814,
+      },
+    ],
+    src: "./gearcalculator/helms/Iron_med_helm.webp",
+  },
+  {
+    type: "Melee",
+    name: "Iron full helm",
+    defLvl: 1,
+    rangedLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -6,
+        Range: -3,
+      },
+      // Defence
+      {
+        Stab: 4,
+        Slash: 5,
+        Crush: 3,
+        Magic: -1,
+        Range: 4,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 2.721,
+      },
+    ],
+    src: "./gearcalculator/helms/Iron_full_helm.webp",
+  },
+  {
+    type: "Melee",
+    name: "Steel med helm",
+    defLvl: 5,
+    rangedLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -3,
+        Range: 0,
+      },
+      // Defence
+      {
+        Stab: 7,
+        Slash: 8,
+        Crush: 6,
+        Magic: -1,
+        Range: 7,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 1.814,
+      },
+    ],
+    src: "./gearcalculator/helms/Steel_med_helm.webp",
+  },
+  {
+    type: "Melee",
+    name: "Steel full helm",
+    defLvl: 5,
+    rangedLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -6,
+        Range: -3,
+      },
+      // Defence
+      {
+        Stab: 9,
+        Slash: 10,
+        Crush: 7,
+        Magic: -1,
+        Range: 9,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 2.721,
+      },
+    ],
+    src: "./gearcalculator/helms/Steel_full_helm.webp",
+  },
+  {
+    type: "Melee",
+    name: "Black med helm",
+    defLvl: 10,
+    rangedLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -3,
+        Range: 0,
+      },
+      // Defence
+      {
+        Stab: 9,
+        Slash: 10,
+        Crush: 8,
+        Magic: -1,
+        Range: 9,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 1.814,
+      },
+    ],
+    src: "./gearcalculator/helms/Black_med_helm.webp",
+  },
+  {
+    type: "Melee",
+    name: "Black full helm",
+    defLvl: 10,
+    rangedLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: -3,
+        Range: 0,
+      },
+      // Defence
+      {
+        Stab: 9,
+        Slash: 10,
+        Crush: 8,
+        Magic: -1,
+        Range: 9,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        Prayer: 0,
+        Weight: 1.814,
+      },
+    ],
+    src: "./gearcalculator/helms/Black_full_helm.webp",
   },
 
   //////////////
   //  RANGED  //
   //////////////
 
-  {
-    type: "Ranged",
-    name: "Leather cowl",
-    defLvl: 1,
-    rangedLvl: 1,
-    magicLvl: 0,
-    strengthLvl: 0,
-    src: "./gearcalculator/helms/Leather_cowl.webp",
-  },
+  // {
+  //   type: "Ranged",
+  //   name: "Leather cowl",
+  //   defLvl: 1,
+  //   rangedLvl: 1,
+  //   magicLvl: 0,
+  //   strengthLvl: 0,
+  //   src: "./gearcalculator/helms/Leather_cowl.webp",
+  // },
 
-  //////////////
-  //  Magic  //
-  //////////////
+  // //////////////
+  // //  Magic  //
+  // //////////////
 
-  {
-    type: "Magic",
-    name: "Blue Wizard hat",
-    defLvl: 1,
-    rangedLvl: 0,
-    magicLvl: 1,
-    strengthLvl: 0,
-    src: "./gearcalculator/helms/Blue_wizard_hat.webp",
-  },
+  // {
+  //   type: "Magic",
+  //   name: "Blue Wizard hat",
+  //   defLvl: 1,
+  //   rangedLvl: 0,
+  //   magicLvl: 1,
+  //   strengthLvl: 0,
+  //   src: "./gearcalculator/helms/Blue_wizard_hat.webp",
+  // },
 ];
 
 export default HELMS;
