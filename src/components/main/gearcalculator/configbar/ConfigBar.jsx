07148e69946e727b23cebe8d00527c14df559477
0xRomRom <roman0811@live.nl>
2023-12-03 04:29:44 +0100

    Renders state based array

--- src/components/main/gearcalculator/configbar/ConfigBar.jsx
+++ src/components/main/gearcalculator/configbar/ConfigBar.jsx
@@ -1,13 +1,22 @@
 import stl from "./ConfigBar.module.css";
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
+import HELMS from "../../../../utils/gearcalculator/helms";
 
 const ConfigBar = ({ activeSlot }) => {
   const searchRef = useRef(null);
+  const [itemList, setItemList] = useState([]);
+
+  const setActiveItemList = () => {
+    if (activeSlot === "Headpiece") {
+      setItemList(HELMS);
+    }
+  };
 
   useEffect(() => {
     if (searchRef.current) {
       searchRef.current.focus();
     }
+    setActiveItemList();
   }, [activeSlot]);
 
   return (
@@ -20,6 +29,20 @@ const ConfigBar = ({ activeSlot }) => {
             className={stl.searchbar}
             placeholder={`Search ${activeSlot}`}
           />
+          <ul className={stl.resultList}>
+            {itemList.map((item) => {
+              return (
+                <li key={item.src} className={stl.resultItem}>
+                  <img
+                    src={item.src}
+                    alt="Gear item"
+                    className={stl.gearIcon}
+                  />
+                  <span>{item.name}</span>
+                </li>
+              );
+            })}
+          </ul>
         </>
       )}
     </div>
---
+++ src/utils/gearcalculator/helms.js
@@ -0,0 +1,9 @@
+const HELMS = [
+  {
+    type: "melee",
+    name: "Bronze Full Helm",
+    src: "./gearcalculator/helms/Bronze_full_helm.webp",
+  },
+];
+
+export default HELMS;
