71a796a7dc8638d5af8671a4504327e1252f5b3d
0xRomRom <roman0811@live.nl>
2023-12-03 03:34:08 +0100

    All slots properly toggle

--- src/components/main/gearcalculator/GearCalculator.jsx
+++ src/components/main/gearcalculator/GearCalculator.jsx
@@ -17,6 +17,7 @@ const GearCalculator = () => {
   const [activeSlot, setActiveSlot] = useState(null);
 
   const handleSlotChange = (slot) => {
+    if (slot === activeSlot) return setActiveSlot(null);
     setActiveSlot(slot);
   };
 
@@ -38,6 +39,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "cape" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("cape")}
           >
             <img src={cape} className={stl.headSlot} alt="Cape slot" />
           </div>
@@ -45,6 +47,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "necklace" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("necklace")}
           >
             <img src={necklace} className={stl.headSlot} alt="Necklace  slot" />
           </div>
@@ -52,6 +55,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "amunition" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("amunition")}
           >
             <img
               src={amunition}
@@ -65,6 +69,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "weapon" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("weapon")}
           >
             <img src={weapon} className={stl.headSlot} alt="Weapon slot" />
           </div>
@@ -72,6 +77,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "body" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("body")}
           >
             <img src={body} className={stl.headSlot} alt="Body slot" />
           </div>
@@ -79,6 +85,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "shield" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("shield")}
           >
             <img src={shield} className={stl.headSlot} alt="Shield slot" />
           </div>
@@ -88,6 +95,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "legs" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("legs")}
           >
             <img src={legs} className={stl.headSlot} alt="Legs slot" />
           </div>
@@ -97,6 +105,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "gloves" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("gloves")}
           >
             <img src={gloves} className={stl.headSlot} alt="Gloves slot" />
           </div>
@@ -104,6 +113,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "boots" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("boots")}
           >
             <img src={boots} className={stl.headSlot} alt="Boots slot" />
           </div>
@@ -111,6 +121,7 @@ const GearCalculator = () => {
             className={`${stl.slotWrap} ${
               activeSlot === "ring" ? stl.selected : ""
             }`}
+            onClick={() => handleSlotChange("ring")}
           >
             <img src={ring} className={stl.headSlot} alt="Ring slot" />
           </div>
