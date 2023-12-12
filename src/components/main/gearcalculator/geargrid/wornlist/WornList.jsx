9158267490ecffb15da4a1c124bc400a0c97bf60
Roman - <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>
2023-12-12 23:46:08 +0100

    All items show in a list

--- src/components/main/gearcalculator/geargrid/wornlist/WornList.jsx
+++ src/components/main/gearcalculator/geargrid/wornlist/WornList.jsx
@@ -38,6 +38,86 @@ const WornList = ({ bonusState }) => {
           {headImg}
         </li>
       )}
+      {capeImg && (
+        <li className={stl.liItem}>
+          <img src={cape} alt="Cape item slot" className={stl.miniSlotIcon} />
+          {capeImg}
+        </li>
+      )}
+      {neckImg && (
+        <li className={stl.liItem}>
+          <img
+            src={necklace}
+            alt="Neck item slot"
+            className={stl.miniSlotIcon}
+          />
+          {neckImg}
+        </li>
+      )}
+      {amunitionImg && (
+        <li className={stl.liItem}>
+          <img
+            src={amunition}
+            alt="Amunition item slot"
+            className={stl.miniSlotIcon}
+          />
+          {amunitionImg}
+        </li>
+      )}
+      {weaponImg && (
+        <li className={stl.liItem}>
+          <img
+            src={weapon}
+            alt="Weapon item slot"
+            className={stl.miniSlotIcon}
+          />
+          {weaponImg}
+        </li>
+      )}
+      {bodyImg && (
+        <li className={stl.liItem}>
+          <img src={body} alt="Body item slot" className={stl.miniSlotIcon} />
+          {bodyImg}
+        </li>
+      )}
+      {shieldImg && (
+        <li className={stl.liItem}>
+          <img
+            src={shield}
+            alt="Shield item slot"
+            className={stl.miniSlotIcon}
+          />
+          {shieldImg}
+        </li>
+      )}
+      {legsImg && (
+        <li className={stl.liItem}>
+          <img src={legs} alt="Legs item slot" className={stl.miniSlotIcon} />
+          {legsImg}
+        </li>
+      )}
+      {glovesImg && (
+        <li className={stl.liItem}>
+          <img
+            src={gloves}
+            alt="Gloves item slot"
+            className={stl.miniSlotIcon}
+          />
+          {glovesImg}
+        </li>
+      )}
+      {bootsImg && (
+        <li className={stl.liItem}>
+          <img src={boots} alt="Boots item slot" className={stl.miniSlotIcon} />
+          {bootsImg}
+        </li>
+      )}
+      {ringImg && (
+        <li className={stl.liItem}>
+          <img src={ring} alt="Ring item slot" className={stl.miniSlotIcon} />
+          {ringImg}
+        </li>
+      )}
     </ul>
   );
 };
