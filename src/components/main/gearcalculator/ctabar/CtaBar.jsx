dc08da6c4ada1b7daef7ccc3c7a0245639f3f6a0
Roman - <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>
2023-12-13 00:35:45 +0100

    Button description added

--- src/components/main/gearcalculator/ctabar/CtaBar.jsx
+++ src/components/main/gearcalculator/ctabar/CtaBar.jsx
@@ -1,7 +1,30 @@
 import stl from "./CtaBar.module.css";
+import { ImBin } from "react-icons/im";
+import { FaCameraRetro, FaSav } from "react-icons/fa";
+import { FaSave } from "react-icons/fa";
+import { BsFillShareFill } from "react-icons/bs";
 
 const CtaBar = () => {
-  return <div className={stl.ctaBar}></div>;
+  return (
+    <div className={stl.ctaBar}>
+      <button>
+        <ImBin className={stl.ctaIcon} />
+        <span className={stl.infoSpan}>Clear</span>
+      </button>
+      <button>
+        <FaCameraRetro className={stl.ctaIcon} />
+        <span className={stl.infoSpan}>Screenshot</span>
+      </button>
+      <button>
+        <FaSave className={stl.ctaIcon} />
+        <span className={stl.infoSpan}>Save</span>
+      </button>
+      <button>
+        <BsFillShareFill className={stl.ctaIcon} />
+        <span className={stl.infoSpan}>Share</span>
+      </button>
+    </div>
+  );
 };
 
 export default CtaBar;
