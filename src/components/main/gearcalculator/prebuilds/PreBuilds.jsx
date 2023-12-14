39f63c8fe84fc00e9bf0075e5ff405f2c9535b26
0xRomRom <roman0811@live.nl>
2023-12-14 01:24:41 +0100

    Grid tile initiated

--- src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
+++ src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
@@ -6,6 +6,16 @@ const PreBuilds = () => {
       <h2 className={stl.hero}>Prebuilds</h2>
       <div className={stl.freeBox}>
         <h3 className={stl.subHero}>Free</h3>
+        <div className={stl.tilesGrid}>
+          <div className={stl.gridTile}>
+            <img
+              src="./geartiles/Dharok.webp"
+              alt="Dharok Equipment"
+              className={stl.tileGear}
+            />
+            <span className={stl.gearName}>Dharok's</span>
+          </div>
+        </div>
       </div>
       <div className={stl.premiumBox}>
         <h3 className={stl.subHero}>Premium</h3>
