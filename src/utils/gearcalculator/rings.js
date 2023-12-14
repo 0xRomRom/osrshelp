35beb0010a2b812a66f45efd77f33604c5fd3fd3
0xRomRom <roman0811@live.nl>
2023-12-14 18:24:32 +0100

    F2p slots added

--- src/utils/gearcalculator/rings.js
+++ src/utils/gearcalculator/rings.js
@@ -441,6 +441,86 @@ const RINGS = [
   },
   {
     type: "Melee",
+    name: "Ring of life",
+    defLvl: 0,
+    rangedLvl: 0,
+    prayerLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    attackLvl: 0,
+    hitpointsLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      // Defence
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        "Slayer Bonus": 0,
+        Prayer: 0,
+        Weight: 0.006,
+      },
+    ],
+    src: "./gearcalculator/rings/Ring_of_life.webp",
+    slot: "Ring",
+  },
+  {
+    type: "Melee",
+    name: "Ring of recoil",
+    defLvl: 0,
+    rangedLvl: 0,
+    prayerLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    attackLvl: 0,
+    hitpointsLvl: 0,
+    stats: [
+      // Attack
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      // Defence
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      // Other
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        "Slayer Bonus": 0,
+        Prayer: 0,
+        Weight: 0.006,
+      },
+    ],
+    src: "./gearcalculator/rings/Ring_of_recoil.webp",
+    slot: "Ring",
+  },
+  {
+    type: "Melee",
     name: "Granite ring",
     defLvl: 0,
     rangedLvl: 0,
