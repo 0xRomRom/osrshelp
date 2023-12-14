8fb510bfcea92693df417b331ed43a2aecfadc24
0xRomRom <roman0811@live.nl>
2023-12-14 01:39:06 +0100

    Item array added

--- src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
+++ src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
@@ -1,5 +1,7 @@
 import stl from "./PreBuilds.module.css";
 
+import FREEBUILDS from "../../../../utils/gearcalculator/prebuilds/free/freeBuilds";
+
 const PreBuilds = () => {
   return (
     <div className={stl.prebuilds}>
@@ -7,22 +9,14 @@ const PreBuilds = () => {
       <div className={stl.freeBox}>
         <h3 className={stl.subHero}>Free</h3>
         <div className={stl.tilesGrid}>
-          <div className={stl.gridTile}>
-            <img
-              src="./geartiles/Dharok.webp"
-              alt="Dharok Equipment"
-              className={stl.tileGear}
-            />
-            <span className={stl.gearName}>Dharok's</span>
-          </div>
-          <div className={stl.gridTile}>
-            <img
-              src="./geartiles/Verac.webp"
-              alt="Verac Equipment"
-              className={stl.tileGear}
-            />
-            <span className={stl.gearName}>Verac's</span>
-          </div>
+          {FREEBUILDS.map((build) => {
+            return (
+              <div className={stl.gridTile} key={build.src}>
+                <img src={build.src} alt={build.alt} className={stl.tileGear} />
+                <span className={stl.gearName}>{build.name}</span>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div className={stl.premiumBox}>
---
+++ src/utils/gearcalculator/prebuilds/free/freeBuilds.js
@@ -0,0 +1,14 @@
+const FREEBUILDS = [
+  {
+    name: "Dharok's",
+    src: "./geartiles/Dharok.webp",
+    alt: "Dharok's Equipment",
+  },
+  {
+    name: "Verac's",
+    src: "./geartiles/Verac.webp",
+    alt: "Verac's Equipment",
+  },
+];
+
+export default FREEBUILDS;
