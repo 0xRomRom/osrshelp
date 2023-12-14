0fbb43cd019b723f6c03a2e324c6efb1b14ae5d3
0xRomRom <roman0811@live.nl>
2023-12-14 02:11:56 +0100

    Logs map entry

--- src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
+++ src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
@@ -3,6 +3,10 @@ import stl from "./PreBuilds.module.css";
 import FREEBUILDS from "../../../../utils/gearcalculator/prebuilds/free/freeBuilds";
 
 const PreBuilds = () => {
+  const handleGearDisplay = (slot) => {
+    console.log(slot);
+  };
+
   return (
     <div className={stl.prebuilds}>
       <h2 className={stl.hero}>Prebuilds</h2>
@@ -10,8 +14,13 @@ const PreBuilds = () => {
         <h3 className={stl.subHero}>Free</h3>
         <div className={stl.tilesGrid}>
           {FREEBUILDS.map((build) => {
+            const { buildslot } = build;
             return (
-              <div className={stl.gridTile} key={build.src}>
+              <div
+                className={stl.gridTile}
+                key={build.src}
+                onClick={() => handleGearDisplay(buildslot)}
+              >
                 <img src={build.src} alt={build.alt} className={stl.tileGear} />
                 <span className={stl.gearName}>{build.name}</span>
               </div>
--- src/utils/gearcalculator/prebuilds/free/freeBuilds.js
+++ src/utils/gearcalculator/prebuilds/free/freeBuilds.js
@@ -3,11 +3,13 @@ const FREEBUILDS = [
     name: "Dharok's",
     src: "./geartiles/Dharok.webp",
     alt: "Dharok's Equipment",
+    buildslot: "Dharok",
   },
   {
     name: "Verac's",
     src: "./geartiles/Verac.webp",
     alt: "Verac's Equipment",
+    buildslot: "Verac",
   },
 ];
 
