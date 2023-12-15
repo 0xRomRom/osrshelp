c616857d9968e5193e64c5730c813c38990b1341
0xRomRom <roman0811@live.nl>
2023-12-15 21:42:47 +0100

    Login modal layout createdg

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -8,15 +8,20 @@ const SignUp = () => {
         <h1 className={stl.hero}>Sign up or log in</h1>
         <button className={stl.googleCta}>
           <img src={googleIcon} alt="Google logo" className={stl.googleIcon} />
-          Continue with Google
+          Signup with Google
         </button>
         <div className={stl.orBox}>
-          <span>or</span>
+          <div className={stl.border}></div>
+          <span className={stl.or}>or</span>
+          <div className={stl.border}></div>
         </div>
         <form className={stl.loginForm}>
           <input type="text" className={stl.input} placeholder="Email" />
           <input type="password" className={stl.input} placeholder="Password" />
           <button className={stl.createCta}>Create account</button>
+          <span className={stl.loginSpan}>
+            Already have an account? <span className={stl.login}>Log in</span>
+          </span>
         </form>
       </div>
     </div>
