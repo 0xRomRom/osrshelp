c8a4693ff415ab9f8857278f9db58ba62f975758
0xRomRom <roman0811@live.nl>
2023-12-15 18:35:59 +0100

    Form created

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,7 +1,26 @@
 import stl from "./SignUp.module.css";
+import googleIcon from "../../assets/icons/Google.png";
 
 const SignUp = () => {
-  return <div className={stl.signup}>hi</div>;
+  return (
+    <div className={stl.signup}>
+      <div className={stl.modal}>
+        <h1 className={stl.hero}>Sign up or log in</h1>
+        <button className={stl.googleCta}>
+          <img src={googleIcon} alt="Google logo" className={stl.googleIcon} />
+          Continue with Google
+        </button>
+        <div className={stl.orBox}>
+          <span>or</span>
+        </div>
+        <form className={stl.loginForm}>
+          <input type="text" className={stl.input} placeholder="Email" />
+          <input type="password" className={stl.input} placeholder="Password" />
+          <button className={stl.createCta}>Create account</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
