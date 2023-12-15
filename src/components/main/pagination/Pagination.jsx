957d711871bcd77e8b9624a25952bbeeb4dfc310
0xRomRom <roman0811@live.nl>
2023-12-15 07:50:06 +0100

    Login button created and redirects

--- src/components/main/pagination/Pagination.jsx
+++ src/components/main/pagination/Pagination.jsx
@@ -1,24 +1,34 @@
 import stl from "./Pagination.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const Pagination = ({ mainState, subState, setSubState }) => {
+  const navigate = useNavigate();
+
   const clearSubState = () => {
     setSubState(null);
   };
 
   return (
     <div className={stl.paginationBar}>
-      <div className={stl.blueDot}></div>
-      <span className={stl.mainMenu} onClick={clearSubState}>
-        {mainState}
-      </span>
-      {subState && (
-        <>
-          <FontAwesomeIcon icon={faAngleRight} className={stl.arrowRight} />
-          <span className={stl.mainMenu}>{subState}</span>
-        </>
-      )}
+      <div className={stl.leftBar}>
+        <div className={stl.blueDot}></div>
+        <span className={stl.mainMenu} onClick={clearSubState}>
+          {mainState}
+        </span>
+        {subState && (
+          <>
+            <FontAwesomeIcon icon={faAngleRight} className={stl.arrowRight} />
+            <span className={stl.mainMenu}>{subState}</span>
+          </>
+        )}
+      </div>
+      <div className={stl.rightBar}>
+        <button className={stl.loginBtn} onClick={() => navigate("/login")}>
+          Login
+        </button>
+      </div>
     </div>
   );
 };
