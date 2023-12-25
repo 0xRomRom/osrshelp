e8b504a9ac5db16a6682ed60b4798ae832225c94
0xRomRom <roman0811@live.nl>
2023-12-25 03:30:32 +0100

    Premium modal added to gearcalculaotrs

--- src/components/main/gearcalculator/safegearmodal/upgradebanner/UpgradeBanner.jsx
+++ src/components/main/gearcalculator/safegearmodal/upgradebanner/UpgradeBanner.jsx
@@ -1,7 +1,24 @@
 import stl from "./UpgradeBanner.module.css";
 
 const UpgradeBanner = () => {
-  return <div className={stl.upgradeBanner}></div>;
+  return (
+    <div className={stl.upgradeBanner}>
+      <div className={stl.bannerBG}>
+        <img
+          src="./premiummodal/PremiumBG.webp"
+          alt="Oldschool Runescape background"
+          className={stl.modalBgImg}
+        />
+      </div>
+      <div className={stl.bannerContent}>
+        <h3 className={stl.bannerHero}>
+          Explore storage slots <br />
+          as a <span className={stl.rune}>rune</span> user
+        </h3>
+        <button className={stl.cta}>Upgrade</button>
+      </div>
+    </div>
+  );
 };
 
 export default UpgradeBanner;
