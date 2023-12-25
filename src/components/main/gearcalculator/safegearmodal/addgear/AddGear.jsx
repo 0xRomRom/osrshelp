0ee0634d694b9ed1de4a7ab1c39d86e547e748ea
0xRomRom <roman0811@live.nl>
2023-12-25 04:30:33 +0100

    Add gear base layout created

--- src/components/main/gearcalculator/safegearmodal/addgear/AddGear.jsx
+++ src/components/main/gearcalculator/safegearmodal/addgear/AddGear.jsx
@@ -6,7 +6,18 @@ const AddGear = ({ bonusState }) => {
     console.log(bonusState);
   }, [bonusState]);
 
-  return <div className={stl.addGear}></div>;
+  return (
+    <div className={stl.addGear}>
+      <div className={stl.upperBlock}>
+        <div className={stl.gearPreview}></div>
+        <div className={stl.currentSlots}></div>
+      </div>
+      <div className={stl.ctaBar}>
+        <button className={stl.ctaBtn}>Cancel</button>
+        <button className={stl.ctaBtn}>Save</button>
+      </div>
+    </div>
+  );
 };
 
 export default AddGear;
