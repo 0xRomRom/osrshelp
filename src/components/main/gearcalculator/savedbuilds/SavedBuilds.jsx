641c671c8f3401ddf080ae55863e07e0096cc874
0xRomRom <roman0811@live.nl>
2023-12-27 22:35:07 +0100

    Dynamic coloring working

--- src/components/main/gearcalculator/savedbuilds/SavedBuilds.jsx
+++ src/components/main/gearcalculator/savedbuilds/SavedBuilds.jsx
@@ -2,45 +2,71 @@ import stl from "./SavedBuilds.module.css";
 import { CiSquarePlus } from "react-icons/ci";
 import { useEffect } from "react";
 
-const SavedBuilds = ({ savedSlots }) => {
-  // useEffect(() => {
-  //   console.log(savedSlots);
-  // }, [savedSlots]);
+import attLogo from "../../../../assets/skillicons/Attack.webp";
+import rangedLogo from "../../../../assets/skillicons/Ranged.webp";
+import magicLogo from "../../../../assets/skillicons/Magic.webp";
 
-  const slot1 = savedSlots["slot1"] ? savedSlots["slot1"].Setupname : null;
-  const slot2 = savedSlots["slot2"] ? savedSlots["slot2"].Setupname : null;
-  const slot3 = savedSlots["slot3"] ? savedSlots["slot3"].Setupname : null;
-  const slot4 = savedSlots["slot4"] ? savedSlots["slot4"].Setupname : null;
-  const slot5 = savedSlots["slot5"] ? savedSlots["slot5"].Setupname : null;
-  const slot6 = savedSlots["slot6"] ? savedSlots["slot6"].Setupname : null;
-  const slot7 = savedSlots["slot7"] ? savedSlots["slot7"].Setupname : null;
-  const slot8 = savedSlots["slot8"] ? savedSlots["slot8"].Setupname : null;
-  const slot9 = savedSlots["slot9"] ? savedSlots["slot9"].Setupname : null;
-  const slot10 = savedSlots["slot10"] ? savedSlots["slot10"].Setupname : null;
+const SavedBuilds = ({ savedSlots }) => {
+  useEffect(() => {
+    console.log(savedSlots);
+    // console.log(savedSlots["slot1"].Setupname);
+  }, [savedSlots]);
+
+  const gearTypeCheck = (geartype) => {
+    switch (geartype) {
+      case "All":
+        return stl.allStyles;
+      case "Melee":
+        return stl.meleeStyle;
+      case "Ranged":
+        return stl.rangedStyle;
+      case "Magic":
+        return stl.magicStyle;
+      default:
+        return "";
+    }
+  };
+
+  const slot1 = savedSlots["slot1"] ? savedSlots["slot1"] : "";
+  const slot2 = savedSlots["slot2"] ? savedSlots["slot2"] : "";
+  const slot3 = savedSlots["slot3"] ? savedSlots["slot3"] : "";
+  const slot4 = savedSlots["slot4"] ? savedSlots["slot4"] : "";
+  const slot5 = savedSlots["slot5"] ? savedSlots["slot5"] : "";
+  const slot6 = savedSlots["slot6"] ? savedSlots["slot6"] : "";
+  const slot7 = savedSlots["slot7"] ? savedSlots["slot7"] : "";
+  const slot8 = savedSlots["slot8"] ? savedSlots["slot8"] : "";
+  const slot9 = savedSlots["slot9"] ? savedSlots["slot9"] : "";
+  const slot10 = savedSlots["slot10"] ? savedSlots["slot10"] : "";
 
   return (
     <div className={stl.savedBuilds}>
       <h2 className={stl.savedHero}>Saved builds</h2>
       <div className={stl.buildsList}>
-        <div className={stl.buildSlot}>
-          {slot1}
-          {!slot1 && (
+        <div
+          className={`${stl.buildSlot} ${
+            slot1 ? gearTypeCheck(slot1.Geartype) : ""
+          }`}
+        >
+          {slot1 && slot1["Setupname"]}
+          {!slot1["Setupname"] && (
             <div className={stl.empty}>
               <span>Empty</span>
               <CiSquarePlus className={stl.plus} />
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
-          {slot2}
-          {!slot2 && (
+          {slot2 && slot2["Setupname"]}
+          {!slot2["Setupname"] && (
             <div className={stl.empty}>
               <span>Empty</span>
               <CiSquarePlus className={stl.plus} />
             </div>
           )}
         </div>
-        <div className={stl.buildSlot}>
+
+        {/* <div className={stl.buildSlot}>
           {slot3}
           {!slot3 && (
             <div className={stl.empty}>
@@ -49,6 +75,7 @@ const SavedBuilds = ({ savedSlots }) => {
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
           {slot4}
           {!slot4 && (
@@ -58,6 +85,7 @@ const SavedBuilds = ({ savedSlots }) => {
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
           {slot5}
           {!slot5 && (
@@ -67,6 +95,7 @@ const SavedBuilds = ({ savedSlots }) => {
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
           {slot6}
           {!slot6 && (
@@ -76,6 +105,7 @@ const SavedBuilds = ({ savedSlots }) => {
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
           {slot7}
           {!slot7 && (
@@ -85,6 +115,7 @@ const SavedBuilds = ({ savedSlots }) => {
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
           {slot8}
           {!slot8 && (
@@ -94,6 +125,7 @@ const SavedBuilds = ({ savedSlots }) => {
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
           {slot9}
           {!slot9 && (
@@ -103,6 +135,7 @@ const SavedBuilds = ({ savedSlots }) => {
             </div>
           )}
         </div>
+
         <div className={stl.buildSlot}>
           {slot10}
           {!slot10 && (
@@ -111,7 +144,7 @@ const SavedBuilds = ({ savedSlots }) => {
               <CiSquarePlus className={stl.plus} />
             </div>
           )}
-        </div>
+        </div> */}
       </div>
     </div>
   );
