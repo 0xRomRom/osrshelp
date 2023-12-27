3d7bd1e249b12478293b29405f149ee4f55f2223
0xRomRom <roman0811@live.nl>
2023-12-27 21:20:05 +0100

    Gear setups shows boxes

--- src/components/main/gearcalculator/savedbuilds/SavedBuilds.jsx
+++ src/components/main/gearcalculator/savedbuilds/SavedBuilds.jsx
@@ -1,70 +1,116 @@
 import stl from "./SavedBuilds.module.css";
 import { CiSquarePlus } from "react-icons/ci";
+import { useEffect } from "react";
 
-const SavedBuilds = () => {
+const SavedBuilds = ({ savedSlots }) => {
+  // useEffect(() => {
+  //   console.log(savedSlots);
+  // }, [savedSlots]);
+
+  const slot1 = savedSlots["slot1"] ? savedSlots["slot1"].Setupname : null;
+  const slot2 = savedSlots["slot2"] ? savedSlots["slot2"].Setupname : null;
+  const slot3 = savedSlots["slot3"] ? savedSlots["slot3"].Setupname : null;
+  const slot4 = savedSlots["slot4"] ? savedSlots["slot4"].Setupname : null;
+  const slot5 = savedSlots["slot5"] ? savedSlots["slot5"].Setupname : null;
+  const slot6 = savedSlots["slot6"] ? savedSlots["slot6"].Setupname : null;
+  const slot7 = savedSlots["slot7"] ? savedSlots["slot7"].Setupname : null;
+  const slot8 = savedSlots["slot8"] ? savedSlots["slot8"].Setupname : null;
+  const slot9 = savedSlots["slot9"] ? savedSlots["slot9"].Setupname : null;
+  const slot10 = savedSlots["slot10"] ? savedSlots["slot10"].Setupname : null;
+
   return (
     <div className={stl.savedBuilds}>
       <h2 className={stl.savedHero}>Saved builds</h2>
       <div className={stl.buildsList}>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot1}
+          {!slot1 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot2}
+          {!slot2 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot3}
+          {!slot3 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot4}
+          {!slot4 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot5}
+          {!slot5 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot6}
+          {!slot6 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot7}
+          {!slot7 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot8}
+          {!slot8 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot9}
+          {!slot9 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
         <div className={stl.buildSlot}>
-          <div className={stl.empty}>
-            <span>Empty</span>
-            <CiSquarePlus className={stl.plus} />
-          </div>
+          {slot10}
+          {!slot10 && (
+            <div className={stl.empty}>
+              <span>Empty</span>
+              <CiSquarePlus className={stl.plus} />
+            </div>
+          )}
         </div>
       </div>
     </div>
