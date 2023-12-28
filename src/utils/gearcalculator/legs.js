1e5239fdfcabf3fa56a9accbc1920c5bcafc53ba
0xRomRom <roman0811@live.nl>
2023-12-28 17:46:29 +0100

    Bunch more additions

--- src/utils/gearcalculator/legs.js
+++ src/utils/gearcalculator/legs.js
@@ -1,5 +1,42 @@
 const LEGS = [
   {
+    type: "Melee",
+    name: "Angler waders",
+    defLvl: 0,
+    rangedLvl: 0,
+    prayerLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    attackLvl: 0,
+    hitpointsLvl: 0,
+    stats: [
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        "Slayer Bonus": 0,
+        Prayer: 0,
+        Weight: 1,
+      },
+    ],
+    src: "./gearcalculator/legs/Angler_waders.webp",
+    slot: "Legs",
+  },
+  {
     type: "Magic",
     name: "3rd age druidic robe bottoms",
     defLvl: 0,
