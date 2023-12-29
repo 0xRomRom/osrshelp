fd58d73014c0d155ade303c1be505eb86e1cc579
0xRomRom <roman0811@live.nl>
2023-12-29 19:29:09 +0100

    Gear sets added

--- src/utils/gearcalculator/helms.js
+++ src/utils/gearcalculator/helms.js
@@ -1,6 +1,43 @@
 const HELMS = [
   {
     type: "Melee",
+    name: "Farmer's strawhat",
+    defLvl: 0,
+    rangedLvl: 0,
+    prayerLvl: 0,
+    magicLvl: 0,
+    strengthLvl: 0,
+    attackLvl: 0,
+    hitpointsLvl: 0,
+    stats: [
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      {
+        Stab: 0,
+        Slash: 0,
+        Crush: 0,
+        Magic: 0,
+        Range: 0,
+      },
+      {
+        "Melee strength": 0,
+        "Ranged strength": 0,
+        "Magic damage": 0,
+        "Slayer Bonus": 0,
+        Prayer: 0,
+        Weight: 0.907,
+      },
+    ],
+    src: "./gearcalculator/helms/Farmer's_strawhat.webp",
+    slot: "Headpiece",
+  },
+  {
+    type: "Melee",
     name: "Lumberjack hat",
     defLvl: 0,
     rangedLvl: 0,
