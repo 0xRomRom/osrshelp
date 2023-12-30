cdc8004d91daa8a87c42b2499c628cd558cf153c
0xRomRom <roman0811@live.nl>
2023-12-30 18:30:01 +0100

    Money makers type selection buttons setup

--- src/components/main/moneymakers/MoneyMakers.jsx
+++ src/components/main/moneymakers/MoneyMakers.jsx
@@ -1,7 +1,10 @@
 import stl from "./MoneyMakers.module.css";
 import Pagination from "../pagination/Pagination";
+import { useState } from "react";
 
 const MoneyMakers = (props) => {
+  const [activeModal, setActiveModal] = useState("Free");
+
   return (
     <div className={stl.moneymakers}>
       <div className={stl.adBar}>[ Advertisements ]</div>
@@ -10,6 +13,26 @@ const MoneyMakers = (props) => {
         subState={props.subState}
         setSubState={props.setSubState}
       />
+      <div className={stl.modal}>
+        <div className={stl.typeSelection}>
+          <button
+            className={`${stl.typeCta} ${
+              activeModal === "Free" ? stl.actBtn : ""
+            }`}
+            onClick={() => setActiveModal("Free")}
+          >
+            Free User
+          </button>
+          <button
+            className={`${stl.typeCta} ${
+              activeModal === "Rune" ? stl.actBtn : ""
+            }`}
+            onClick={() => setActiveModal("Rune")}
+          >
+            Rune User
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
