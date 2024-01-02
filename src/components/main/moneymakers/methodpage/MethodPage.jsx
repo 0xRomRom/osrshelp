2711fa18a8fea4ee532b4006a41baf01a48e0e41
0xRomRom <roman0811@live.nl>
2024-01-02 22:47:09 +0100

    Routing back and forth working

--- src/components/main/moneymakers/methodpage/MethodPage.jsx
+++ src/components/main/moneymakers/methodpage/MethodPage.jsx
@@ -1,9 +1,22 @@
 import stl from "./MethodPage.module.css";
+import Pagination from "../../pagination/Pagination";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const MethodPage = () => {
+  const navigate = useNavigate();
+
+  const navigateBack = () => {
+    navigate("/moneymakers");
+  };
+
   return (
     <div className={stl.methodpage}>
-      <span>Hello world</span>
+      <div className={stl.adBar}>[ Advertisements ]</div>
+      <Pagination />
+      <div className={stl.modal}>
+        <FaLongArrowAltLeft className={stl.closeBtn} onClick={navigateBack} />
+      </div>
     </div>
   );
 };
