aad4383b5c21d71937695ef8c207f9f0276bc96a
0xRomRom <roman0811@live.nl>
2024-01-03 01:06:35 +0100

    All skill icons added to object

--- src/utils/skilliconobject.js
+++ src/utils/skilliconobject.js
@@ -1,6 +1,27 @@
 const SKILLICONOBJECT = {
+  Attack: "./skillicons/Attack.webp",
+  Agility: "./skillicons/Agility.webp",
+  Construction: "./skillicons/Construction.webp",
+  Cooking: "./skillicons/Cooking.webp",
+  Crafting: "./skillicons/Crafting.webp",
+  Defence: "./skillicons/Defence.webp",
+  Farming: "./skillicons/Farming.webp",
+  Firemaking: "./skillicons/Firemaking.webp",
+  Fishing: "./skillicons/Fishing.webp",
+  Fletching: "./skillicons/Fletching.webp",
+  Herblore: "./skillicons/Herblore.webp",
+  Hitpoints: "./skillicons/Hitpoints.webp",
   Hunter: "./skillicons/Hunter.webp",
+  Magic: "./skillicons/Magic.webp",
+  Mining: "./skillicons/Mining.webp",
+  Prayer: "./skillicons/Prayer.webp",
+  Ranged: "./skillicons/Ranged.webp",
+  Runecraft: "./skillicons/Runecraft.webp",
+  Slayer: "./skillicons/Slayer.webp",
+  Smithing: "./skillicons/Smithing.webp",
+  Strength: "./skillicons/Strength.webp",
   Thieving: "./skillicons/Thieving.webp",
+  Woodcutting: "./skillicons/Woodcutting.webp",
 };
 
 export default SKILLICONOBJECT;
