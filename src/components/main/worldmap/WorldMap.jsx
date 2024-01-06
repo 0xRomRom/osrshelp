0a0a54a78498f41c2f157e3e60d22ac090914a06
0xRomRom <roman0811@live.nl>
2024-01-06 18:08:17 +0100

    Fullscreen toggle working

--- src/components/main/worldmap/WorldMap.jsx
+++ src/components/main/worldmap/WorldMap.jsx
@@ -2,13 +2,20 @@ import stl from "./WorldMap.module.css";
 import Pagination from "../pagination/Pagination";
 
 import { MapContainer, ImageOverlay } from "react-leaflet";
+import { RiFullscreenFill } from "react-icons/ri";
+import { useState } from "react";
 
 const WorldMap = (props) => {
+  const [fullScreen, setFullScreen] = useState(false);
   // Define the bounds for your image
   const bounds = [
-    [-200, -300],
+    [0, 0],
     [400, 400],
   ]; // Replace with your image's bounds
+
+  const handleFullScreen = () => {
+    setFullScreen(!fullScreen);
+  };
 
   return (
     <div className={stl.worldmap}>
@@ -18,15 +25,16 @@ const WorldMap = (props) => {
         subState={props.subState}
         setSubState={props.setSubState}
       />
-      <div className={stl.mapContainers}>
-        <MapContainer
-          center={[20, 190]}
-          zoom={3}
-          style={{ height: "100%", width: "100%", position: "relative" }} // Adjust dimensions as needed
-        >
+      <div
+        className={`${stl.mapContainers} ${fullScreen ? stl.fullScreen : ""}`}
+      >
+        <MapContainer center={[70, 250]} zoom={4} className={stl.mapBox}>
           <ImageOverlay url="./worldmap/Worldmap.png" bounds={bounds} />
-          {/* Add your draggable elements or markers here */}
         </MapContainer>
+        <RiFullscreenFill
+          className={stl.fullScreenBtn}
+          onClick={handleFullScreen}
+        />
       </div>
     </div>
   );
