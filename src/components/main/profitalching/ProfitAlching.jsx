b1f33cb165b43553b8d6097d4b60fb294e1fdebe
0xRomRom <roman0811@live.nl>
2024-01-08 00:39:38 +0100

    Base grid created

--- src/components/main/profitalching/ProfitAlching.jsx
+++ src/components/main/profitalching/ProfitAlching.jsx
@@ -10,7 +10,30 @@ const ProfitAlching = ({ mainState, subState, setSubState }) => {
         setSubState={setSubState}
         subState={subState}
       />
-      <div className={stl.modalWrap}></div>
+      <div className={stl.modalWrap}>
+        <div className={stl.heroBlock}>
+          <h1 className={stl.hero}>
+            <img
+              src="./staves/Staff_of_fire.webp"
+              alt="Staff of fire"
+              className={stl.heroImg}
+            />{" "}
+            Profit Alching
+          </h1>
+          <p className={stl.descPar}>
+            Calculations in the table below are based on realtime G.E. prices.
+          </p>
+        </div>
+        <div className={stl.itemsGrid}>
+          <div className={stl.configRow}>
+            <span>Icon</span>
+            <span>Item</span>
+            <span>Price</span>
+            <span>Alch Price</span>
+            <span>Profit</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
