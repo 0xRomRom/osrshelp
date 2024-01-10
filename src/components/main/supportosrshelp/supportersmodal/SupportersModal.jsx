d68f9bf6f02d51b3167ad586c7583141ec282888
0xRomRom <roman0811@live.nl>
2024-01-11 00:33:17 +0100

    Mock list created

--- src/components/main/supportosrshelp/supportersmodal/SupportersModal.jsx
+++ src/components/main/supportosrshelp/supportersmodal/SupportersModal.jsx
@@ -1,7 +1,23 @@
 import stl from "./SupportersModal.module.css";
 
 const SupportersModal = () => {
-  return <div className={stl.supportersmodal}></div>;
+  return (
+    <div className={stl.supportersmodal}>
+      <h2 className={stl.supportersHero}>Supporters</h2>
+      <div className={stl.supportersList}>
+        <div className={stl.supporter}>
+          <img src="./random/Mod.webp" alt="Mod" className={stl.modCrown} />
+          <span className={stl.userName}>King Rom II</span>
+          <span className={stl.amount}>x3</span>
+          <img
+            src="./foods/Lobster.webp"
+            alt="Lobster"
+            className={stl.lobsterImg}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SupportersModal;
