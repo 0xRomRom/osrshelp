f5e8767734580d948bd5997d2a44e4b8af4cd713
0xRomRom <roman0811@live.nl>
2024-01-10 01:21:49 +0100

    Back to home button created

--- src/components/about/faq/Faq.jsx
+++ src/components/about/faq/Faq.jsx
@@ -1,78 +1,12 @@
 import stl from "./Faq.module.css";
-
+import FAQS from "../../../utils/faqs";
+import { FaLongArrowAltLeft } from "react-icons/fa";
 import HomeButton from "../../../utils/homebutton/HomeButton";
 import { useState } from "react";
-
-const FAQS = {
-  0: {
-    text: ["Welcome to OSRS Help's Frequently Asked Questions."],
-  },
-  1: {
-    text: [
-      "OSRS Help's goal is to provide the best user interface & user experience when calculating your ingame goals.",
-      "Other utility sites are boring, old or simply ugly. Yes I said it.",
-      "While providing an intuitive experience, we did not skip over the aesthetics of our tools.",
-      "We hope you enjoy them as much as we enjoy creating them!",
-    ],
-  },
-  2: {
-    text: [
-      "OSRS Help is build and maintained by me, a single software developer and Runescape geek from The Netherlands!",
-      "I'm leveraging my technical skills to contribute to the OSRS community by providing best in class tooling.",
-      "I could have been writing bots, instead we're building the greatest utility site of all!",
-    ],
-  },
-  3: {
-    text: [
-      "Lifetime for only $9.99:",
-      "~ Ad free experience",
-      "~ Access to premium features",
-      "~ Access to future premium features",
-      "~ Access to hidden content",
-      "~ Supports the upkeep of OSRS Help",
-    ],
-  },
-  4: {
-    text: [
-      "Certainly!",
-      "Head over to our contact page and submit the form with your suggestion.",
-      "User feedback is highly appreciated and every submission will be treated as such.",
-    ],
-  },
-  5: {
-    text: [
-      "Navigate to our Report Issue page.",
-      "Submit the form and please specify your issue details.",
-      "We won't reply to your email, unless additional information is required.",
-      "Thanks in advance!",
-    ],
-  },
-  6: {
-    text: [
-      "Visit our Contact page to find our contact information.",
-      "Feel free to submit the form with any question you might have.",
-    ],
-  },
-  7: {
-    text: [
-      "Thanks for asking, that's nice of you!",
-      "If you want to contribute to the growth of OSRS Help, the easiest way is by letting others know about our tools.",
-      "Additionally if you're feeling rich, supporting our site financially is also highly appreciated.",
-      "We will always be free to use, thanks to our supporters.",
-    ],
-  },
-  8: {
-    text: [
-      "Simply no data is collected.",
-      "We only use cookies to improve user experience by storing user configured settings.",
-      "No data is recorded, stored or shared with 3rd parties.",
-      "We take your privacy serious and don't aim to export any personal information.",
-      "Ever.",
-    ],
-  },
-};
+import { useNavigate } from "react-router-dom";
 
 const FAQ = () => {
+  const navigate = useNavigate();
   const [faqState, setFaqState] = useState(FAQS[0]);
 
   const updateFaqState = (newState) => {
@@ -90,6 +24,10 @@ const FAQ = () => {
     <div className={stl.faq}>
       <HomeButton />
       <div className={stl.faqModal}>
+        <span className={stl.homeSpan} onClick={() => navigate("/")}>
+          <FaLongArrowAltLeft className={stl.homeArrow} />
+          <span className={stl.homeTxt}>Home</span>
+        </span>
         <h1 className={stl.faqHero}>FAQ</h1>
         <div className={stl.questionsBlock}>
           <span
---
+++ src/utils/faqs.js
@@ -0,0 +1,70 @@
+const FAQS = {
+  0: {
+    text: ["Welcome to OSRS Help's Frequently Asked Questions."],
+  },
+  1: {
+    text: [
+      "OSRS Help's goal is to provide the best user interface/experience when calculating your ingame goals.",
+      "Other utility sites are boring, old or simply ugly. Yes I said it.",
+      "While providing an intuitive experience, we did not skip over the aesthetics of our tools.",
+      "We hope you enjoy them as much as we enjoy creating them!",
+    ],
+  },
+  2: {
+    text: [
+      "OSRS Help is build and maintained by me, a single software developer and Runescape geek from The Netherlands!",
+      "I'm leveraging my technical skills to contribute to the OSRS community by providing best in class tooling.",
+      "I could have been writing bots, instead we're building the greatest utility site of all!",
+    ],
+  },
+  3: {
+    text: [
+      "Lifetime for only $9.99:",
+      "~ Ad free experience",
+      "~ Access to premium features",
+      "~ Access to future premium features",
+      "~ Access to hidden content",
+      "~ Supports the upkeep of OSRS Help",
+    ],
+  },
+  4: {
+    text: [
+      "Certainly!",
+      "Head over to our contact page and submit the form with your suggestion.",
+      "User feedback is highly appreciated and every submission will be treated as such.",
+    ],
+  },
+  5: {
+    text: [
+      "Navigate to our Report Issue page.",
+      "Submit the form and please specify your issue details.",
+      "We won't reply to your email, unless additional information is required.",
+      "Thanks in advance!",
+    ],
+  },
+  6: {
+    text: [
+      "Visit our Contact page to find our contact information.",
+      "Feel free to submit the form with any questions you might have.",
+    ],
+  },
+  7: {
+    text: [
+      "Thanks for asking, that's nice of you!",
+      "If you want to contribute to the growth of OSRS Help, the easiest way is by letting others know about our tools.",
+      "Additionally if you're feeling rich, supporting our site financially is also highly appreciated.",
+      "We will always be free to use, thanks to our supporters.",
+    ],
+  },
+  8: {
+    text: [
+      "Simply no data is collected.",
+      "We only use cookies to improve user experience by storing user configured settings.",
+      "No data is recorded, stored or shared with any 3rd parties.",
+      "We take your privacy serious and don't aim to export any personal information.",
+      "Ever.",
+    ],
+  },
+};
+
+export default FAQS;
