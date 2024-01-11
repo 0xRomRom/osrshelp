e4275d37a8fe78e6f500410bcc48bcf8aceb45c7
0xRomRom <roman0811@live.nl>
2024-01-11 04:50:40 +0100

    Base contact modal created

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,47 @@
 import stl from "./Contact.module.css";
 import HomeButton from "../../utils/homebutton/HomeButton";
+import { FaLongArrowAltLeft } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
+  const navigate = useNavigate();
   return (
     <div className={stl.contact}>
       <HomeButton />
+      <div className={stl.contactModal}>
+        <span className={stl.homeSpan} onClick={() => navigate("/")}>
+          <FaLongArrowAltLeft className={stl.homeArrow} />
+          <span className={stl.homeTxt}>Home</span>
+        </span>
+        <h1 className={stl.contactHero}>Contact</h1>
+        <form className={stl.innerModal}>
+          <div className={stl.subjectBar}>
+            <span className={stl.inputSpan}>Subject</span>
+            <input
+              type="text"
+              className={stl.inputStl}
+              placeholder="What do you want to let us know?"
+            />
+          </div>
+          <div className={stl.subjectBar}>
+            <span className={stl.inputSpan}>Email</span>
+            <div className={stl.inputWrapper}>
+              <input
+                type="email"
+                className={stl.inputStl}
+                placeholder="osrsuser@email.com"
+              />
+              <button className={stl.anonBtn}>Anon</button>
+            </div>
+          </div>
+          <div className={stl.textAreaWrap}>
+            <textarea className={stl.textAreaInput}></textarea>
+          </div>
+          <div className={stl.submitBox}>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
