a518eeeb0bae395f031ab4dfb166f254bc8b767a
0xRomRom <roman0811@live.nl>
2024-01-11 05:36:06 +0100

    Autofill issues for email input resolved

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,30 @@ import stl from "./Contact.module.css";
 import HomeButton from "../../utils/homebutton/HomeButton";
 import { FaLongArrowAltLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Contact = () => {
+  const [anonForm, setAnonForm] = useState(false);
+
+  const [formState, setFormState] = useState({
+    subject: "",
+    email: "",
+    message: "",
+  });
+
+  const updateFormState = (key, val) => {
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        [key]: val,
+      };
+    });
+  };
+
+  const submitForm = () => {
+    console.log(formState);
+  };
+
   const navigate = useNavigate();
   return (
     <div className={stl.contact}>
@@ -21,24 +43,39 @@ const Contact = () => {
               type="text"
               className={stl.inputStl}
               placeholder="What do you want to let us know?"
+              onChange={(e) => updateFormState("subject", e.target.value)}
             />
           </div>
           <div className={stl.subjectBar}>
             <span className={stl.inputSpan}>Email</span>
             <div className={stl.inputWrapper}>
-              <input
-                type="email"
-                className={stl.inputStl}
-                placeholder="osrsuser@email.com"
-              />
-              <button className={stl.anonBtn}>Anon</button>
+              {anonForm && (
+                <input
+                  type="email"
+                  className={stl.inputStl}
+                  placeholder="osrsuser@email.com"
+                />
+              )}
+              <button
+                className={stl.anonBtn}
+                onClick={() => setAnonForm(!anonForm)}
+                style={{ left: anonForm ? "initial" : "0" }}
+              >
+                Anon
+              </button>
             </div>
           </div>
+          <span className={stl.inputSpan}>Message</span>
           <div className={stl.textAreaWrap}>
-            <textarea className={stl.textAreaInput}></textarea>
+            <textarea
+              className={stl.textAreaInput}
+              placeholder="Leave your message here"
+            ></textarea>
           </div>
           <div className={stl.submitBox}>
-            <button>Submit</button>
+            <button className={stl.submitCta} onClick={submitForm}>
+              Submit
+            </button>
           </div>
         </form>
       </div>
