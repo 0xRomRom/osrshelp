8469164cf77509bd3504a37240a3f0a8ee02886e
0xRomRom <roman0811@live.nl>
2024-01-12 04:57:34 +0100

    Got word and linebreaks working correctl

--- src/components/main/home/runechat/RuneChat.jsx
+++ src/components/main/home/runechat/RuneChat.jsx
@@ -1,10 +1,11 @@
 import stl from "./RuneChat.module.css";
 import { LuSendHorizonal } from "react-icons/lu";
 import supabase from "../../../../utils/supabase/supabase";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { AuthContext } from "../../../../utils/authprovider/AuthProvider";
 
 const RuneChat = () => {
+  const outputBottom = useRef(null);
   const { userID } = useContext(AuthContext);
   const [userMessage, setUserMessage] = useState("");
   const [inserted, setInserted] = useState(false);
@@ -27,11 +28,12 @@ const RuneChat = () => {
       const { error } = await supabase.from("runechat").insert([
         {
           uid: userID,
-          username: "King Rom II",
+          username: "KKKKKKKKKKKK",
           chatmsg: userMessage,
           timestamp: now,
         },
       ]);
+      outputBottom.current.scrollTop = outputBottom.current.scrollHeight - 10;
       setInserted(true);
 
       if (error) {
@@ -43,50 +45,71 @@ const RuneChat = () => {
   };
 
   useEffect(() => {
-    const fetchMessages = async () => {
-      const { data, error } = await supabase
-        .from("runechat")
-        .select()
-        .limit(20);
-      if (error) {
-        console.error("Error fetching messages:", error);
-      } else {
-        console.log(data);
-        data.sort((a, b) => {
-          // Convert timestamps to seconds for comparison
-          let timeA = a.timestamp
-            .split(":")
-            .reduce(
-              (acc, time, index) =>
-                acc + parseInt(time) * Math.pow(60, 2 - index),
-              0
-            );
-          let timeB = b.timestamp
-            .split(":")
-            .reduce(
-              (acc, time, index) =>
-                acc + parseInt(time) * Math.pow(60, 2 - index),
-              0
-            );
+    if (inserted || currentChat.length === 0) {
+      const fetchMessages = async () => {
+        const { data, error } = await supabase
+          .from("runechat")
+          .select()
+          .order("id", { ascending: false })
+          .limit(50);
+        if (error) {
+          console.error("Error fetching messages:", error);
+        } else {
+          data.sort((a, b) => {
+            // Convert timestamps to seconds for comparison
+            let timeA = a.timestamp
+              .split(":")
+              .reduce(
+                (acc, time, index) =>
+                  acc + parseInt(time) * Math.pow(60, 2 - index),
+                0
+              );
+            let timeB = b.timestamp
+              .split(":")
+              .reduce(
+                (acc, time, index) =>
+                  acc + parseInt(time) * Math.pow(60, 2 - index),
+                0
+              );
 
-          return timeA - timeB;
-        });
+            return timeA - timeB;
+          });
 
-        // const reversed = data.sort((a, b) => b.timestamp - a.timestamp);
-        // console.log(reversed);
-        // console.log(reversed);
-        setCurrentChat(data);
-        setInserted(false);
-        // setMessages(data);
-      }
-    };
-    fetchMessages();
+          setCurrentChat(data);
+          setInserted(false);
+        }
+      };
+      fetchMessages();
+    }
   }, [inserted, currentChat.length]);
+
+  useEffect(() => {
+    supabase
+      .channel("runechat")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "runechat",
+        },
+        (payload) => {
+          // console.log(payload);
+          // const newMessage = payload.new;
+          // setCurrentChat([...currentChat, newMessage]);
+
+          setInserted(true);
+
+          // console.log(newMessage);
+        }
+      )
+      .subscribe();
+  }, [currentChat]);
 
   return (
     <div className={stl.modal}>
       <h2 className={stl.runechat}>Runechat</h2>
-      <div className={stl.chatOutput}>
+      <div className={stl.chatOutput} ref={outputBottom}>
         {currentChat
           .map((chat, index) => {
             return (
