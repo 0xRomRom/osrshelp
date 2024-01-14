c5c87df742b6cdb29a89ea5da861c41dbfb1a7d8
0xRomRom <roman0811@live.nl>
2024-01-15 00:03:33 +0100

    Submissions layed out

--- src/components/main/home/updatepoll/submissionsstate/SubmissionsState.jsx
+++ src/components/main/home/updatepoll/submissionsstate/SubmissionsState.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
 import stl from "./SubmissionsState.module.css";
 
 const SubmissionsState = () => {
+  const [submission, setSubmission] = useState(null);
+
+  const updateSubmissionState = (e) => {
+    setSubmission(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(submission);
+  }, [submission]);
   return (
     <div className={stl.submissionsstate}>
       <h2 className={stl.hero}>Submissions</h2>
@@ -12,6 +22,7 @@ const SubmissionsState = () => {
           type="text"
           className={stl.formInput}
           placeholder="Ex. Pyramid Plunder calculator"
+          onChange={updateSubmissionState}
         />
         <button className={stl.submitCta}>Vote</button>
       </form>
