c565fd878d843e6e7e837e8a5d73813d76da28c1
0xRomRom <roman0811@live.nl>
2024-01-14 23:54:26 +0100

    Base layout for submissions first view created

--- src/components/main/home/updatepoll/submissionsstate/SubmissionsState.jsx
+++ src/components/main/home/updatepoll/submissionsstate/SubmissionsState.jsx
@@ -1,7 +1,22 @@
 import stl from "./SubmissionsState.module.css";
 
 const SubmissionsState = () => {
-  return <div className={stl.submissionsstate}></div>;
+  return (
+    <div className={stl.submissionsstate}>
+      <h2 className={stl.hero}>Submissions</h2>
+      <form className={stl.submitForm}>
+        <span className={stl.inputSpan}>
+          What feature does OSRS Help need next?
+        </span>
+        <input
+          type="text"
+          className={stl.formInput}
+          placeholder="Ex. Pyramid Plunder calculator"
+        />
+        <button className={stl.submitCta}>Vote</button>
+      </form>
+    </div>
+  );
 };
 
 export default SubmissionsState;
