38b23eef2a5cb86b7e5b6a5df5fdebc5de6f599a
0xRomRom <roman0811@live.nl>
2024-01-15 05:45:28 +0100

    Blog maps entries dynamically

--- src/components/main/home/blog/Blog.jsx
+++ src/components/main/home/blog/Blog.jsx
@@ -2,6 +2,16 @@ import { useState } from "react";
 import stl from "./Blog.module.css";
 import { FaArrowDownLong } from "react-icons/fa6";
 
+const blogEntries = [
+  {
+    title: "OSRS Help Release",
+    copy: "The official release of OSRS Help is here! Join us as we delve into the wealth of features that OSRSHelp brings to the table. From skill calculators and gear guides to money making methods and community driven updates, OSRSHelp is not merely a website but a vibrant hub where you get ahead of your competition!",
+  },
+  {
+    title: "Agility Pyramid Calculator",
+    copy: "By high demand, development of the Agility Pyramid Calculator just finished! Profit calculation based on average exp rate is included, along with expected XP rates per agility level.",
+  },
+];
 const btnStates = ["All", "Features", "Misc"];
 const Blog = () => {
   const [activeFilter, setActiveFilter] = useState("All");
@@ -11,39 +21,25 @@ const Blog = () => {
       <h2 className={stl.title}>Blog</h2>
 
       <div className={stl.updatesBox}>
-        <div className={stl.feature}>
-          <h2 className={stl.featureTitle}>OSRS Help Release</h2>
-          <p className={stl.featureCopy}>
-            The official release of OSRS Help is here! Join us as we delve into
-            the wealth of features that OSRSHelp brings to the table. From skill
-            calculators and gear guides to money making methods and community
-            driven updates, OSRSHelp is not merely a website but a vibrant hub
-            where you get ahead of your competition!
-          </p>
-          <span className={stl.readMore}>
-            Read more <FaArrowDownLong className={stl.readArrow} />
-          </span>
-        </div>
-        <div className={stl.feature}>
-          <h2 className={stl.featureTitle}>OSRS Help Release</h2>
-          <p className={stl.featureCopy}>
-            The official release of OSRS Help is here! Join us as we delve into
-            the wealth of features that OSRSHelp brings to the table. From skill
-            calculators and gear guides to money making methods and community
-            driven updates, OSRSHelp is not merely a website but a vibrant hub
-            where you get ahead of your competition!
-          </p>
-          <span className={stl.readMore}>
-            Read more <FaArrowDownLong className={stl.readArrow} />
-          </span>
-        </div>
+        {blogEntries.map((blog) => {
+          return (
+            <div className={stl.feature}>
+              <h2 className={stl.featureTitle}>{blog.title}</h2>
+              <p className={stl.featureCopy}>{blog.copy}</p>
+              <span className={stl.readMore}>
+                Read more <FaArrowDownLong className={stl.readArrow} />
+              </span>
+            </div>
+          );
+        })}
       </div>
       <div className={stl.filterBox}>
         {btnStates.map((btn) => (
           <button
+            key={btn}
             onClick={() => setActiveFilter(btn)}
             className={`${stl.filterBtn} ${
-              activeFilter == btn ? stl.activeBtn : ""
+              activeFilter === btn ? stl.activeBtn : ""
             }`}
           >
             {btn}
