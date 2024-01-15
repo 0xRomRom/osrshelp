0ccbdf38abf8a910b9b697820bee7ea2474f9dd6
0xRomRom <roman0811@live.nl>
2024-01-15 22:51:35 +0100

    Skillgrid is rendered dynamically

--- src/utils/skillsgridlist.js
+++ src/utils/skillsgridlist.js
@@ -9,102 +9,106 @@ const SKILLSGRIDLIST = [
     icon: "./skillicons/Defence.webp",
     path: "/skillcalculators/defence",
   },
-  //   { name: "Strength", icon: strengthIcon, path: "/skillcalculators/strength" },
-  //   {
-  //     name: "Hitpoints",
-  //     icon: hitpointsIcon,
-  //     path: "/skillcalculators/hitpoints",
-  //   },
-  //   {
-  //     name: "Ranged",
-  //     icon: rangedIcon,
-  //     path: "/skillcalculators/ranged",
-  //   },
-  //   {
-  //     name: "Prayer",
-  //     icon: prayerIcon,
-  //     path: "/skillcalculators/prayer",
-  //   },
-  //   {
-  //     name: "Magic",
-  //     icon: magicIcon,
-  //     path: "/skillcalculators/magic",
-  //   },
-  //   {
-  //     name: "Cooking",
-  //     icon: cookingIcon,
-  //     path: "/skillcalculators/cooking",
-  //   },
-  //   {
-  //     name: "Woodcutting",
-  //     icon: woodcuttingIcon,
-  //     path: "/skillcalculators/woodcutting",
-  //   },
-  //   {
-  //     name: "Fletching",
-  //     icon: fletchingIcon,
-  //     path: "/skillcalculators/fletching",
-  //   },
-  //   {
-  //     name: "Fishing",
-  //     icon: fishingIcon,
-  //     path: "/skillcalculators/fishing",
-  //   },
-  //   {
-  //     name: "Firemaking",
-  //     icon: firemakingIcon,
-  //     path: "/skillcalculators/firemaking",
-  //   },
-  //   {
-  //     name: "Crafting",
-  //     icon: craftingIcon,
-  //     path: "/skillcalculators/crafting",
-  //   },
-  //   {
-  //     name: "Smithing",
-  //     icon: smithingIcon,
-  //     path: "/skillcalculators/smithing",
-  //   },
-  //   {
-  //     name: "Mining",
-  //     icon: miningIcon,
-  //     path: "/skillcalculators/mining",
-  //   },
-  //   {
-  //     name: "Herblore",
-  //     icon: herbloreIcon,
-  //     path: "/skillcalculators/herblore",
-  //   },
-  //   {
-  //     name: "Agility",
-  //     icon: agilityIcon,
-  //     path: "/skillcalculators/agility",
-  //   },
-  //   {
-  //     name: "Thieving",
-  //     icon: thievingIcon,
-  //     path: "/skillcalculators/thieving",
-  //   },
-  //   {
-  //     name: "Farming",
-  //     icon: farmingIcon,
-  //     path: "/skillcalculators/farming",
-  //   },
-  //   {
-  //     name: "Runecraft",
-  //     icon: runecraftingIcon,
-  //     path: "/skillcalculators/runecraft",
-  //   },
-  //   {
-  //     name: "Hunter",
-  //     icon: hunterIcon,
-  //     path: "/skillcalculators/hunter",
-  //   },
-  //   {
-  //     name: "Construction",
-  //     icon: constructionIcon,
-  //     path: "/skillcalculators/construction",
-  //   },
+  {
+    name: "Strength",
+    icon: "./skillicons/Strength.png",
+    path: "/skillcalculators/strength",
+  },
+  {
+    name: "Hitpoints",
+    icon: "./skillicons/Hitpoints.webp",
+    path: "/skillcalculators/hitpoints",
+  },
+  {
+    name: "Ranged",
+    icon: "./skillicons/Ranged.webp",
+    path: "/skillcalculators/ranged",
+  },
+  {
+    name: "Prayer",
+    icon: "./skillicons/Prayer.webp",
+    path: "/skillcalculators/prayer",
+  },
+  {
+    name: "Magic",
+    icon: "./skillicons/Magic.webp",
+    path: "/skillcalculators/magic",
+  },
+  {
+    name: "Cooking",
+    icon: "./skillicons/Cooking.webp",
+    path: "/skillcalculators/cooking",
+  },
+  {
+    name: "Woodcutting",
+    icon: "./skillicons/Woodcutting.webp",
+    path: "/skillcalculators/woodcutting",
+  },
+  {
+    name: "Fletching",
+    icon: "./skillicons/Fletching.webp",
+    path: "/skillcalculators/fletching",
+  },
+  {
+    name: "Fishing",
+    icon: "./skillicons/Fishing.webp",
+    path: "/skillcalculators/fishing",
+  },
+  {
+    name: "Firemaking",
+    icon: "./skillicons/Firemaking.png",
+    path: "/skillcalculators/firemaking",
+  },
+  {
+    name: "Crafting",
+    icon: "./skillicons/Crafting.webp",
+    path: "/skillcalculators/crafting",
+  },
+  {
+    name: "Smithing",
+    icon: "./skillicons/Smithing.webp",
+    path: "/skillcalculators/smithing",
+  },
+  {
+    name: "Mining",
+    icon: "./skillicons/mining.webp",
+    path: "/skillcalculators/mining",
+  },
+  {
+    name: "Herblore",
+    icon: "./skillicons/Herblore.webp",
+    path: "/skillcalculators/herblore",
+  },
+  {
+    name: "Agility",
+    icon: "./skillicons/Agility.webp",
+    path: "/skillcalculators/agility",
+  },
+  {
+    name: "Thieving",
+    icon: "./skillicons/Thieving.webp",
+    path: "/skillcalculators/thieving",
+  },
+  {
+    name: "Farming",
+    icon: "./skillicons/Farming.webp",
+    path: "/skillcalculators/farming",
+  },
+  {
+    name: "Runecraft",
+    icon: "./skillicons/Runecraft.webp",
+    path: "/skillcalculators/runecraft",
+  },
+  {
+    name: "Hunter",
+    icon: "./skillicons/Hunter.webp",
+    path: "/skillcalculators/hunter",
+  },
+  {
+    name: "Construction",
+    icon: "./skillicons/Construction.webp",
+    path: "/skillcalculators/construction",
+  },
 ];
 
 export default SKILLSGRIDLIST;
