2f475175fc5868aa90cb22c4adfe8c68475341c7
0xRomRom <roman0811@live.nl>
2024-01-26 05:27:35 +0100

    Basic scraping working

--- src/components/main/xptable/XPTable.jsx
+++ src/components/main/xptable/XPTable.jsx
@@ -2,8 +2,49 @@ import stl from "./XPTable.module.css";
 import Pagination from "../pagination/Pagination";
 import XPCalculator from "./xpcalculator/XPCalculator";
 import { arrayXpTable } from "../../../utils/playerStats";
+import cheerio from "cheerio";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const XPTable = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    if (data.length > 0) {
+      console.log(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fetcher = await fetch("http://localhost:3001/api/shooting-stars");
+        const result = await fetcher.json();
+        console.log(result);
+
+        return;
+        const url = "https://osrsportal.com/shooting-stars-tracker";
+        const response = await axios.get(url);
+
+        if (response.status === 200) {
+          const $ = cheerio.load(response.data);
+
+          // Replace '.your-selector' with the appropriate CSS selector for the data you want to scrape
+          const scrapedData = $(".styledd-table").text();
+
+          // Update state with the scraped data
+          setData(scrapedData);
+        } else {
+          console.error("Failed to fetch data");
+        }
+      } catch (error) {
+        console.error("Error:", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className={stl.xptable}>
       <div className={stl.adBar}>[ Advertisements ]</div>
