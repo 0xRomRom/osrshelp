289cc7bc630e0edd680a42cce0eca0a44e2d7bf4
0xRomRom <roman0811@live.nl>
2024-01-26 03:56:48 +0100

    Xp grid restyled

--- src/components/main/xptable/XPTable.jsx
+++ src/components/main/xptable/XPTable.jsx
@@ -11,22 +11,24 @@ const XPTable = () => {
       <div className={stl.levelsWrap}>
         <h1 className={stl.modalTitle}>OSRS XP Table</h1>
         <XPCalculator />
-        <div className={stl.xpGrid}>
-          <div className={stl.configRow}>
-            <span>Level</span>
-            <span>Exp</span>
-            <span>Difference</span>
-          </div>
-          <div className={stl.levelGrid}>
-            {arrayXpTable.map((level, index) => {
-              return (
-                <div className={stl.flexGrid} key={index}>
-                  <span>{level.level}</span>
-                  <span>{level.expRequired.toLocaleString()}</span>
-                  <span>{level.expDifference.toLocaleString()}</span>
-                </div>
-              );
-            })}
+        <div className={stl.xpGridWrapper}>
+          <div className={stl.xpGrid}>
+            <div className={stl.configRow}>
+              <span>Level</span>
+              <span>Exp</span>
+              <span>Difference</span>
+            </div>
+            <div className={stl.levelGrid}>
+              {arrayXpTable.map((level, index) => {
+                return (
+                  <div className={stl.flexGrid} key={index}>
+                    <span>{level.level}</span>
+                    <span>{level.expRequired.toLocaleString()}</span>
+                    <span>{level.expDifference.toLocaleString()}</span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
