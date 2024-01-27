99ac0c52402838b61c9da6f33a8eb8803c016b43
0xRomRom <roman0811@live.nl>
2024-01-27 04:41:05 +0100

    Dragging items kinda working (;

--- src/components/main/inventorybuilder/InventoryGrid/InventoryGrid.jsx
+++ src/components/main/inventorybuilder/InventoryGrid/InventoryGrid.jsx
@@ -1,15 +1,33 @@
 import stl from "./InventoryGrid.module.css";
+import Draggable, { DraggableCore } from "react-draggable";
+import { useRef } from "react";
 
 const InventoryGrid = ({ currentGrid, setCurrentGrid }) => {
+  const nodeRef = useRef(null);
   const deleteGridItem = (item) => {
-    console.log(+Object.keys(item)[0]);
-    const toDelete = +Object.keys(item)[0];
+    const deleteIndex = +Object.keys(item)[0];
 
     let newGrid = [...currentGrid];
-    console.log(newGrid[toDelete - 1]);
-    newGrid[toDelete - 1] = { [toDelete]: "" };
-    console.log(newGrid);
+    newGrid[deleteIndex - 1] = { [deleteIndex]: "" };
+
     setCurrentGrid(newGrid);
+  };
+
+  const eventLogger = (e, data) => {
+    console.log("Event: ", e);
+    console.log("Data: ", data);
+  };
+
+  const handleStart = () => {
+    // Handle start logic
+  };
+
+  const handleDrag = () => {
+    // Handle drag logic
+  };
+
+  const handleStop = () => {
+    // Handle stop logic
   };
 
   return (
@@ -17,15 +35,29 @@ const InventoryGrid = ({ currentGrid, setCurrentGrid }) => {
       <div className={stl.innerWrap}>
         {currentGrid.map((item, index) => {
           return (
-            <div
-              className={stl.itemSlot}
+            <Draggable
               key={index}
-              onClick={() => deleteGridItem(item)}
+              nodeRef={nodeRef}
+              onDragStart={handleStart}
+              onDrag={eventLogger}
+              onDragEnd={handleStop}
+              grid={[68, 60]}
+              allowAnyClick={Boolean}
             >
-              {item[index + 1] && (
-                <img src={item[index + 1]} alt="Item" className={stl.tileImg} />
-              )}
-            </div>
+              <div
+                className={stl.itemSlot}
+                onDoubleClick={() => deleteGridItem(item)}
+                ref={nodeRef}
+              >
+                {item[index + 1] && (
+                  <img
+                    src={item[index + 1]}
+                    alt="Item"
+                    className={stl.tileImg}
+                  />
+                )}
+              </div>
+            </Draggable>
           );
         })}
       </div>
