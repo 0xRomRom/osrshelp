89dae5f5ef7eaf7c97c19971de144430194b1240
0xRomRom <roman0811@live.nl>
2024-01-29 21:15:45 +0100

    Tabular system created

--- src/components/main/inventorybuilder/ItemGalery/ItemGalery.jsx
+++ src/components/main/inventorybuilder/ItemGalery/ItemGalery.jsx
@@ -1,18 +1,30 @@
 import stl from "./ItemGalery.module.css";
 import member from "../../../../assets/icons/Member.webp";
+import { useState } from "react";
 
 const ItemGalery = () => {
+  const [activeGallery, setActiveGallery] = useState("Armor");
   return (
     <div className={stl.itemGalery}>
       <div className={stl.toggleBar}>
-        <button className={stl.toggleCta}>
+        <button
+          className={`${stl.toggleCta} ${
+            activeGallery === "Armor" ? stl.activeCta : ""
+          }`}
+          onClick={() => setActiveGallery("Armor")}
+        >
           <img
             src="https://oldschool.runescape.wiki/images/Dharok%27s_helm.png?3043c"
             alt="Dharok's Helm"
             className={stl.btnIcon}
           />
         </button>
-        <button className={stl.toggleCta}>
+        <button
+          className={`${stl.toggleCta} ${
+            activeGallery === "Potions" ? stl.activeCta : ""
+          }`}
+          onClick={() => setActiveGallery("Potions")}
+        >
           <img
             src="https://oldschool.runescape.wiki/images/Saradomin_brew%284%29.png"
             alt="Saradomin brew"
@@ -20,22 +32,37 @@ const ItemGalery = () => {
           />
         </button>
 
-        <button className={stl.toggleCta}>
+        <button
+          className={`${stl.toggleCta} ${
+            activeGallery === "Food" ? stl.activeCta : ""
+          }`}
+          onClick={() => setActiveGallery("Food")}
+        >
           <img
             src="https://oldschool.runescape.wiki/images/Shark.png"
             alt="Shark"
             className={stl.btnIcon}
           />
         </button>
-        <button className={stl.toggleCta}>
+        <button
+          className={`${stl.toggleCta} ${
+            activeGallery === "Runes" ? stl.activeCta : ""
+          }`}
+          onClick={() => setActiveGallery("Runes")}
+        >
           <img
             src="https://oldschool.runescape.wiki/images/Blood_rune.png?3d4c6"
             alt="Blood rune"
             className={stl.btnIcon}
           />
         </button>
-        <button className={`${stl.toggleCta} ${stl.lastCta}`}>
-          <img src={member} alt="Blood rune" className={stl.btnIcon} />
+        <button
+          className={`${stl.toggleCta} ${stl.lastCta} ${
+            activeGallery === "Favorites" ? stl.activeCta : ""
+          }`}
+          onClick={() => setActiveGallery("Favorites")}
+        >
+          <img src={member} alt="Favorites" className={stl.btnIcon} />
         </button>
       </div>
     </div>
