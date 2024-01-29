464b8aeb5c026aa8a7cb6d914594a92b66b91b0e
0xRomRom <roman0811@live.nl>
2024-01-29 21:47:14 +0100

    Amount bar toggles amounts

--- src/components/main/inventorybuilder/ItemGalery/AmountBar/AmountBar.jsx
+++ src/components/main/inventorybuilder/ItemGalery/AmountBar/AmountBar.jsx
@@ -1,7 +1,39 @@
 import stl from "./AmountBar.module.css";
+import { useState } from "react";
 
 const AmountBar = () => {
-  return <div className={stl.amountbar}></div>;
+  const [amount, setAmount] = useState("1");
+
+  return (
+    <div className={stl.amountbar}>
+      <button
+        className={`${stl.amountCta} ${amount === "1" ? stl.activeCta : ""}`}
+        onClick={() => setAmount("1")}
+      >
+        1
+      </button>
+      <button
+        className={`${stl.amountCta} ${amount === "5" ? stl.activeCta : ""}`}
+        onClick={() => setAmount("5")}
+      >
+        5
+      </button>
+      <button
+        className={`${stl.amountCta} ${amount === "10" ? stl.activeCta : ""}`}
+        onClick={() => setAmount("10")}
+      >
+        10
+      </button>
+      <button
+        className={`${stl.amountCta} ${stl.lastCta} ${
+          amount === "Fill" ? stl.activeCta : ""
+        }`}
+        onClick={() => setAmount("Fill")}
+      >
+        Fill
+      </button>
+    </div>
+  );
 };
 
 export default AmountBar;
