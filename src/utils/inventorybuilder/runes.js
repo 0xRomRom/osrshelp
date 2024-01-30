b12d30aeeed3dd2f19a08ec7ed00f8f4afb4abd3
0xRomRom <roman0811@live.nl>
2024-01-30 05:34:58 +0100

    All runes added

--- src/utils/inventorybuilder/runes.js
+++ src/utils/inventorybuilder/runes.js
@@ -15,6 +15,74 @@ const RUNES = [
     name: "Earth rune",
     src: "https://oldschool.runescape.wiki/images/Earth_rune.png?0b998",
   },
+  {
+    name: "Fire rune",
+    src: "https://oldschool.runescape.wiki/images/Fire_rune.png?0b998",
+  },
+  {
+    name: "Body rune",
+    src: "https://oldschool.runescape.wiki/images/Body_rune.png?0b998",
+  },
+  {
+    name: "Cosmic rune",
+    src: "https://oldschool.runescape.wiki/images/Cosmic_rune.png?0b998",
+  },
+  {
+    name: "Chaos rune",
+    src: "https://oldschool.runescape.wiki/images/Chaos_rune.png?0b998",
+  },
+  {
+    name: "Nature rune",
+    src: "https://oldschool.runescape.wiki/images/Nature_rune.png?0b998",
+  },
+  {
+    name: "Law rune",
+    src: "https://oldschool.runescape.wiki/images/Law_rune.png?0b998",
+  },
+  {
+    name: "Death rune",
+    src: "https://oldschool.runescape.wiki/images/Death_rune.png?0b998",
+  },
+  {
+    name: "Astral rune",
+    src: "https://oldschool.runescape.wiki/images/Astral_rune.png?0b998",
+  },
+  {
+    name: "Blood rune",
+    src: "https://oldschool.runescape.wiki/images/Blood_rune.png?0b998",
+  },
+  {
+    name: "Soul rune",
+    src: "https://oldschool.runescape.wiki/images/Soul_rune.png?0b998",
+  },
+  {
+    name: "Wrath rune",
+    src: "https://oldschool.runescape.wiki/images/Wrath_rune.png?0b998",
+  },
+  {
+    name: "Mist rune",
+    src: "https://oldschool.runescape.wiki/images/Mist_rune.png?0b998",
+  },
+  {
+    name: "Dust rune",
+    src: "https://oldschool.runescape.wiki/images/Dust_rune.png?0b998",
+  },
+  {
+    name: "Mud rune",
+    src: "https://oldschool.runescape.wiki/images/Mud_rune.png?0b998",
+  },
+  {
+    name: "Smoke rune",
+    src: "https://oldschool.runescape.wiki/images/Smoke_rune.png?0b998",
+  },
+  {
+    name: "Steam rune",
+    src: "https://oldschool.runescape.wiki/images/Steam_rune.png?0b998",
+  },
+  {
+    name: "Lava rune",
+    src: "https://oldschool.runescape.wiki/images/Lava_rune.png?0b998",
+  },
 ];
 
 export default RUNES;
