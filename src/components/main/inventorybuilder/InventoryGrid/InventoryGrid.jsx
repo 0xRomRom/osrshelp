54d22895309eceec8d791f8670b2ab8ebf98ba11
0xRomRom <roman0811@live.nl>
2024-01-31 02:24:31 +0100

    Swapping between tiles working

--- src/components/main/inventorybuilder/InventoryGrid/InventoryGrid.jsx
+++ src/components/main/inventorybuilder/InventoryGrid/InventoryGrid.jsx
@@ -43,23 +43,28 @@ const InventoryGrid = ({
     }
 
     const temp = newGrid[selectedIndex];
+
+    // Start item
     newGrid[selectedIndex] = newGrid[currentTarget];
+    // Target item
     newGrid[currentTarget] = temp;
 
     const tempKey = Object.keys(newGrid[selectedIndex])[0];
 
+    //Start item
     newGrid[selectedIndex] = {
       [Object.keys(newGrid[currentTarget])[0]]: Object.values(
         newGrid[selectedIndex]
       )[0],
       noted: Object.values(newGrid[currentTarget])[1],
-      amount: Object.values(newGrid[currentTarget])[2],
+      amount: Object.values(newGrid[selectedIndex])[2],
     };
 
+    // Target item
     newGrid[currentTarget] = {
       [tempKey]: Object.values(newGrid[currentTarget])[0],
-      noted: Object.values(newGrid[currentTarget])[1],
-      amount: Object.values(newGrid[currentTarget])[2],
+      noted: Object.values(newGrid[selectedIndex])[1], // Swap with the amount value
+      amount: Object.values(newGrid[currentTarget])[2], // Swap with the noted value
     };
 
     setCurrentGrid(newGrid);
