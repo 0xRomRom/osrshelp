83b1053f3818e33d24f09f6e51580a0a7e161e66
0xRomRom <roman0811@live.nl>
2024-01-31 21:12:24 +0100

    Storage tabs created

--- src/components/main/inventorybuilder/ItemGalery/ActiveGallery/Favorites/Favorites.jsx
+++ src/components/main/inventorybuilder/ItemGalery/ActiveGallery/Favorites/Favorites.jsx
@@ -1,10 +1,32 @@
 import stl from "./Favorites.module.css";
 import PremiumBanner from "./premiumbanner/PremiumBanner";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../../../../utils/authprovider/AuthProvider";
+
+const tabNames = ["Tab 1", "Tab 2", "Tab 3", "Tab 4"];
 
 const Favorites = () => {
+  const { premiumUser } = useContext(AuthContext);
+  const [activeTab, setActiveTab] = useState(1);
+
   return (
     <div className={stl.favorites}>
-      <PremiumBanner />
+      {!premiumUser && <PremiumBanner />}
+      <div className={stl.favoritesWrap}>
+        <div className={stl.tabBar}>
+          {tabNames.map((tab, index) => (
+            <button
+              key={index}
+              className={`${stl.tabCta} ${
+                activeTab === index + 1 ? stl.activeCta : ""
+              }`}
+              onClick={() => setActiveTab(index + 1)}
+            >
+              {tab}
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
