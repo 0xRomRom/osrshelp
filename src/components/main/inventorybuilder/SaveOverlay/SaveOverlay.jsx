75bebda03a94506af2e918db5a41a8bc89add117
0xRomRom <roman0811@live.nl>
2024-02-03 01:59:35 +0100

    Base layout for modal created

--- src/components/main/inventorybuilder/SaveOverlay/SaveOverlay.jsx
+++ src/components/main/inventorybuilder/SaveOverlay/SaveOverlay.jsx
@@ -10,6 +10,19 @@ const SaveOverlay = ({ setSavingInventory }) => {
     <div className={stl.saveoverlay}>
       <div className={stl.modal}>
         <FaLongArrowAltLeft className={stl.closeBtn} onClick={closeModal} />
+        <div className={stl.upperBlock}>
+          <div className={stl.inventoryGrid}>
+            <div className={stl.gridTile}></div>
+          </div>
+          <div className={stl.addBar}>
+            <input
+              type="text"
+              className={stl.nameInput}
+              placeholder="New inventory name"
+            />
+          </div>
+          <div className={stl.savedBar}></div>
+        </div>
       </div>
     </div>
   );
