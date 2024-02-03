e201f64456aaf179d21f75d3d1616cca59cb54a3
0xRomRom <roman0811@live.nl>
2024-02-03 19:55:51 +0100

    Fetches saved inventories

--- src/components/main/inventorybuilder/SavedInventorys/SavedInventorys.jsx
+++ src/components/main/inventorybuilder/SavedInventorys/SavedInventorys.jsx
@@ -1,11 +1,43 @@
 import stl from "./SavedInventorys.module.css";
 import PremiumBanner from "./PremiumBanner/PremiumBanner";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../../../utils/authprovider/AuthProvider";
 import { BsGearFill } from "react-icons/bs";
+import supabase from "../../../../utils/supabase/supabase";
 
 const SavedInventorys = ({ setSavingInventory }) => {
-  const { premiumUser } = useContext(AuthContext);
+  const { premiumUser, userID } = useContext(AuthContext);
+  const [invList, setInvList] = useState([]);
+
+  useEffect(() => {
+    const listFetcher = async () => {
+      try {
+        const { data, error } = await supabase
+          .from("saved_inventories")
+          .select("*")
+          .eq("uid", userID);
+
+        if (error) {
+          throw new Error(error);
+        }
+        if (data) {
+          const result = JSON.parse(data[0].saved_invs);
+          console.log(result);
+          setInvList(result);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    if (invList.length === 0) {
+      listFetcher();
+    }
+  });
+
+  useEffect(() => {
+    console.log(invList);
+  }, [invList]);
 
   return (
     <div className={stl.savedinventorys}>
@@ -17,6 +49,17 @@ const SavedInventorys = ({ setSavingInventory }) => {
               className={stl.gearicon}
               onClick={() => setSavingInventory(true)}
             />
+          </div>
+          <div className={stl.storedInvsList}>
+            {invList.length > 0 && (
+              <>
+                {invList.map((item, index) => (
+                  <div className={stl.listItem} key={index}>
+                    {item[index]}
+                  </div>
+                ))}
+              </>
+            )}
           </div>
         </div>
       )}
