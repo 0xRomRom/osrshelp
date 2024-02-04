41a4f0ceed88986da0a11c3255541849cc5aa7b4
0xRomRom <roman0811@live.nl>
2024-02-04 07:26:57 +0100

    Bottom adbar conditionally rendered

--- src/utils/adbars/bottomadbar/BottomAdBar.jsx
+++ src/utils/adbars/bottomadbar/BottomAdBar.jsx
@@ -1,7 +1,23 @@
 import stl from "./BottomAdBar.module.css";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../../authprovider/AuthProvider";
 
 const BottomAdBar = () => {
-  return <div className={stl.adbar}>[ Advertisements ]</div>;
+  const { premiumUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    console.log(premiumUser);
+  }, [premiumUser]);
+  return (
+    <>
+      <div
+        className={stl.adbar}
+        style={{ height: premiumUser === true ? "10px" : "105px" }}
+      >
+        {premiumUser === false && "[Advertisements]"}
+      </div>
+    </>
+  );
 };
 
 export default BottomAdBar;
