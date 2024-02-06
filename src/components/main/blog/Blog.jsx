ddb54cf7128a87fc9b0458b532d9e8b6d2d6873e
0xRomRom <roman0811@live.nl>
2024-02-06 04:31:19 +0100

    Blog preview tile created

--- src/components/main/blog/Blog.jsx
+++ src/components/main/blog/Blog.jsx
@@ -2,6 +2,7 @@ import stl from "./Blog.module.css";
 import TopAdBar from "../../../utils/adbars/topadbar/TopAdBar";
 import BottomAdBar from "../../../utils/adbars/bottomadbar/BottomAdBar";
 import Pagination from "../pagination/Pagination";
+import { FaArrowDownLong } from "react-icons/fa6";
 
 const Blog = () => {
   return (
@@ -19,6 +20,19 @@ const Blog = () => {
                 alt="Blog banner"
                 className={stl.blogTileImg}
               />
+            </div>
+            <div className={stl.textWrapper}>
+              <h2 className={stl.blogItemHero}>Official launch of OSRS Help</h2>
+              <p className={stl.blogTeaser}>
+                The official release of OSRS Help is here! Join us as we delve
+                into the wealth of features that OSRS Help brings to the table.
+                From skill calculators and gear guides to money making methods
+                and community driven updates, OSRS Help is not merely a website
+                but a vibrant hub where you get ahead of your competition!"
+              </p>
+              <span className={stl.readMore}>
+                Read more <FaArrowDownLong className={stl.readArrow} />
+              </span>
             </div>
           </div>
         </div>
