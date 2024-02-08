00a032b0c0fb781957000be8ea0ab2a7cb7fb1eb
0xRomRom <roman0811@live.nl>
2024-02-08 08:49:11 +0100

    Cta btns mapped

--- src/utils/admin/Admin.jsx
+++ src/utils/admin/Admin.jsx
@@ -1,52 +1,34 @@
 import stl from "./Admin.module.css";
 import { useState } from "react";
 
+const ctaS = [
+  {
+    path: "Home",
+  },
+  { path: "Users" },
+  { path: "Contact" },
+  { path: "Issues" },
+  { path: "Blog" },
+];
+
 const Admin = () => {
   const [activeBtn, setActiveBtn] = useState("Home");
   return (
     <div className={stl.admin}>
       <div className={stl.modal}>
         <div className={stl.toggleBar}>
-          <button
-            className={`${stl.toggleCta} ${
-              activeBtn === "Home" ? stl.activeCta : ""
-            }`}
-            onClick={() => setActiveBtn("Home")}
-          >
-            Home
-          </button>
-          <button
-            className={`${stl.toggleCta} ${
-              activeBtn === "Users" ? stl.activeCta : ""
-            }`}
-            onClick={() => setActiveBtn("Users")}
-          >
-            Users
-          </button>
-          <button
-            className={`${stl.toggleCta} ${
-              activeBtn === "Contact" ? stl.activeCta : ""
-            }`}
-            onClick={() => setActiveBtn("Contact")}
-          >
-            Contact
-          </button>
-          <button
-            className={`${stl.toggleCta} ${
-              activeBtn === "Issues" ? stl.activeCta : ""
-            }`}
-            onClick={() => setActiveBtn("Issues")}
-          >
-            Issues
-          </button>
-          <button
-            className={`${stl.toggleCta} ${
-              activeBtn === "Blog" ? stl.activeCta : ""
-            }`}
-            onClick={() => setActiveBtn("Blog")}
-          >
-            Blog
-          </button>
+          {ctaS.map((cta) => {
+            return (
+              <button
+                className={`${stl.toggleCta} ${
+                  activeBtn === cta.path ? stl.activeCta : ""
+                }`}
+                onClick={() => setActiveBtn(cta.path)}
+              >
+                {cta.path}
+              </button>
+            );
+          })}
         </div>
       </div>
     </div>
