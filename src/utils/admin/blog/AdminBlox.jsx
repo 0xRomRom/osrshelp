f69b900e46f85b2d697b32b3f8f1ce5a48f81aae
0xRomRom <roman0811@live.nl>
2024-02-17 03:44:03 +0100

    Renders current blogs

--- src/utils/admin/blog/AdminBlox.jsx
+++ src/utils/admin/blog/AdminBlox.jsx
@@ -1,8 +1,31 @@
 import stl from "./AdminBlog.module.css";
 import { useState } from "react";
+import supabase from "../../../utils/supabase/supabase";
+import { useEffect } from "react";
 
 const AdminBlog = () => {
   const [openTab, setOpenTab] = useState("Current");
+  const [blogList, setBlogList] = useState([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      try {
+        const { data, error } = await supabase.from("blog_posts").select("*");
+        console.log(data);
+        if (error) {
+          throw new Error(error);
+        }
+
+        setBlogList(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    if (blogList.length === 0) {
+      fetchBlogs();
+    }
+  }, []);
 
   return (
     <div className={stl.adminblog}>
@@ -28,7 +51,11 @@ const AdminBlog = () => {
       <div className={stl.admincontainers}>
         {openTab === "Current" && (
           <div className={stl.currentBlog}>
-            <div className={stl.blogstate}></div>
+            <div className={stl.blogstate}>
+              {blogList.map((blog) => {
+                return <div className={stl.blogItem}>{blog.title}</div>;
+              })}
+            </div>
           </div>
         )}
       </div>
