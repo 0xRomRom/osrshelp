743095929687e583e026d328fadcabdbc764f5c2
0xRomRom <roman0811@live.nl>
2024-02-17 21:59:10 +0100

    Vote results calculated and rendered from object

--- src/utils/admin/poll/overview/Overview.jsx
+++ src/utils/admin/poll/overview/Overview.jsx
@@ -34,7 +34,35 @@ const Overview = () => {
           throw new Error(error);
         }
 
-        setVoteResults(data);
+        const tempObj = {
+          1: 0,
+          2: 0,
+          3: 0,
+          4: 0,
+          5: 0,
+        };
+        data.forEach((item) => {
+          const vote = item.uservote;
+          console.log(item);
+
+          if (vote === 1) {
+            tempObj[1] = tempObj[1] + 1;
+          }
+          if (vote === 2) {
+            tempObj[2] = tempObj[2] + 1;
+          }
+          if (vote === 3) {
+            tempObj[3] = tempObj[3] + 1;
+          }
+          if (vote === 4) {
+            tempObj[4] = tempObj[4] + 1;
+          }
+          if (vote === 5) {
+            tempObj[5] = tempObj[5] + 1;
+          }
+        });
+
+        setVoteResults(tempObj);
       } catch (err) {
         console.error(err);
       }
@@ -52,15 +80,32 @@ const Overview = () => {
     <div className={stl.overview}>
       <div className={stl.submissions}>
         <span className={stl.herospan}>Poll Submissions</span>
-        {submissionsList.map((item) => {
-          return <span className={stl.submission}>{item.submission}</span>;
+        {submissionsList.map((item, index) => {
+          return (
+            <span className={stl.submission} key={index}>
+              {item.submission}
+            </span>
+          );
         })}
       </div>
       <div className={stl.voteresults}>
         <span className={stl.herospan}>Vote Results</span>
-        {/* {Object.entries(submissionsList).map((item) => {
-          return <span className={stl.submission}>{item}</span>;
-        })} */}
+
+        <span className={stl.submission}>
+          Q1 : {voteResults && voteResults[1]}
+        </span>
+        <span className={stl.submission}>
+          Q2 : {voteResults && voteResults[2]}
+        </span>
+        <span className={stl.submission}>
+          Q3 : {voteResults && voteResults[3]}
+        </span>
+        <span className={stl.submission}>
+          Q4 : {voteResults && voteResults[4]}
+        </span>
+        <span className={stl.submission}>
+          Q5 : {voteResults && voteResults[5]}
+        </span>
       </div>
     </div>
   );
