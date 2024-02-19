50994ae0eb283d18f0a14f3fcdcdece66ae17993
0xRomRom <roman0811@live.nl>
2024-02-19 02:50:09 +0100

    Runecredits created

--- src/utils/userprofile/runecredits/RuneCredits.jsx
+++ src/utils/userprofile/runecredits/RuneCredits.jsx
@@ -1,9 +1,17 @@
 import stl from "./RuneCredits.module.css";
+import mills from "../../../assets/icons/Mills.webp";
 
 const RuneCredits = () => {
   return (
     <div className={stl.tile}>
       <span className={stl.graySpan}>Rune Credits</span>
+      <div className={stl.creditsBox}>
+        <span className={stl.creditsCount}>
+          {" "}
+          <img src={mills} alt="Money pile" className={stl.moneyPile} /> 250
+        </span>
+        <span>Credits</span>
+      </div>
     </div>
   );
 };
