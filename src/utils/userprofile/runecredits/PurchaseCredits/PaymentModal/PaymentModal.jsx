e5e74f519cf399fb5b853945006c910899c7ff51
vandersarroman <vandersarroman@gmail.com>
2024-02-26 21:58:44 +0100

    Payment modal created

---
+++ src/utils/userprofile/runecredits/PurchaseCredits/PaymentModal/PaymentModal.jsx
@@ -0,0 +1,7 @@
+import stl from "./PaymentModal.module.css";
+
+const PaymentModal = () => {
+  return <div className={stl.paymentmodal}></div>;
+};
+
+export default PaymentModal;
--- src/utils/userprofile/runecredits/PurchaseCredits/PurchaseCredits.jsx
+++ src/utils/userprofile/runecredits/PurchaseCredits/PurchaseCredits.jsx
@@ -1,56 +1,81 @@
 import stl from "./PurchaseCredits.module.css";
 import { FaLongArrowAltLeft } from "react-icons/fa";
+import { useState } from "react";
+import PaymentModal from "./PaymentModal/PaymentModal";
 
 const PurchaseCredits = ({ setPurchasingCredits }) => {
+  const [purchaseAmount, setPurchaseAmount] = useState(null);
+
+  const handleBackArrow = () => {
+    if (purchaseAmount) {
+      setPurchaseAmount(null);
+      return;
+    }
+    setPurchasingCredits(false);
+    setPurchaseAmount(null);
+  };
+
   return (
     <div className={stl.purchasecredits}>
       <div className={stl.closeRow}>
         <FaLongArrowAltLeft
           className={stl.closeArrow}
-          onClick={() => setPurchasingCredits(false)}
+          onClick={handleBackArrow}
         />
       </div>
-      <div className={stl.plansRow}>
-        <div className={`${stl.card} ${stl.card1}`}>
-          <div className={stl.amountBox}>
-            <h2 className={stl.amountHero}>
-              1000 <span className={stl.creditsSpan}>credits</span>
-            </h2>
-            <span className={stl.cost}>$9.99</span>
+      {!purchaseAmount && (
+        <div className={stl.plansRow}>
+          <div
+            className={`${stl.card} ${stl.card1}`}
+            onClick={() => setPurchaseAmount(1000)}
+          >
+            <div className={stl.amountBox}>
+              <h2 className={stl.amountHero}>
+                1000 <span className={stl.creditsSpan}>credits</span>
+              </h2>
+              <span className={stl.cost}>$9.99</span>
+            </div>
+            <img
+              src="https://oldschool.runescape.wiki/images/Coins_250.png?c2755"
+              className={stl.moneyPile}
+              alt="Oldschool Runescape Money"
+            />
           </div>
-          <img
-            src="https://oldschool.runescape.wiki/images/Coins_250.png?c2755"
-            className={stl.moneyPile}
-            alt="Oldschool Runescape Money"
-          />
-        </div>
-        <div className={`${stl.card} ${stl.card2}`}>
-          <div className={stl.amountBox}>
-            <h2 className={stl.amountHero}>
-              3000 <span className={stl.creditsSpan}>credits</span>
-            </h2>
-            <span className={stl.cost}>$24.99</span>
+          <div
+            className={`${stl.card} ${stl.card2}`}
+            onClick={() => setPurchaseAmount(3000)}
+          >
+            <div className={stl.amountBox}>
+              <h2 className={stl.amountHero}>
+                3000 <span className={stl.creditsSpan}>credits</span>
+              </h2>
+              <span className={stl.cost}>$24.99</span>
+            </div>
+            <img
+              src="https://oldschool.runescape.wiki/images/Coins_1000.png?978c8"
+              className={stl.moneyPile}
+              alt="Oldschool Runescape Money"
+            />
           </div>
-          <img
-            src="https://oldschool.runescape.wiki/images/Coins_1000.png?978c8"
-            className={stl.moneyPile}
-            alt="Oldschool Runescape Money"
-          />
-        </div>
-        <div className={`${stl.card} ${stl.card3}`}>
-          <div className={stl.amountBox}>
-            <h2 className={stl.amountHero}>
-              7000 <span className={stl.creditsSpan}>credits</span>
-            </h2>
-            <span className={stl.cost}>$49.99</span>
+          <div
+            className={`${stl.card} ${stl.card3}`}
+            onClick={() => setPurchaseAmount(7000)}
+          >
+            <div className={stl.amountBox}>
+              <h2 className={stl.amountHero}>
+                7000 <span className={stl.creditsSpan}>credits</span>
+              </h2>
+              <span className={stl.cost}>$49.99</span>
+            </div>
+            <img
+              src="https://oldschool.runescape.wiki/images/Coins_10000.png?7fa38"
+              className={stl.moneyPile}
+              alt="Oldschool Runescape Money"
+            />
           </div>
-          <img
-            src="https://oldschool.runescape.wiki/images/Coins_10000.png?7fa38"
-            className={stl.moneyPile}
-            alt="Oldschool Runescape Money"
-          />
         </div>
-      </div>
+      )}
+      {purchaseAmount && <PaymentModal />}
     </div>
   );
 };
