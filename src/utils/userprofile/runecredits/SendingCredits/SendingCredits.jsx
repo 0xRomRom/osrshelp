bfb3000a360bb2242bcb3f052edbf869c862d3d6
vandersarroman <vandersarroman@gmail.com>
2024-02-27 03:12:28 +0100

    Send modal configured besides db logic

--- src/utils/userprofile/runecredits/SendingCredits/SendingCredits.jsx
+++ src/utils/userprofile/runecredits/SendingCredits/SendingCredits.jsx
@@ -1,16 +1,34 @@
 import stl from "./SendingCredits.module.css";
 import { IoSend } from "react-icons/io5";
+import { useState } from "react";
 
 const SendingCredits = ({ setSendingCredits }) => {
+  const [amountToSend, setAmountToSend] = useState("");
+  const [receiverName, setReceiverName] = useState("");
+
+  const handleCreditTransfer = () => {};
+
   return (
     <div className={stl.sendingcredits}>
       <div className={stl.sendWrap}>
         <span className={stl.graySpan}>Send </span>
-        <input type="number" placeholder="100" className={stl.inputStl} />
+        <input
+          type="number"
+          placeholder="100"
+          className={stl.inputStl}
+          value={amountToSend}
+          onChange={(e) => setAmountToSend(e.target.value)}
+        />
       </div>
       <div className={stl.sendWrap}>
         <span className={stl.graySpan}>To</span>
-        <input type="text" className={stl.inputStl} placeholder="King Rom II" />
+        <input
+          type="text"
+          className={stl.inputStl}
+          placeholder="King Rom II"
+          value={receiverName}
+          onChange={(e) => setReceiverName(e.target.value)}
+        />
       </div>
       <div className={stl.sendWrap}>
         <button
@@ -19,7 +37,7 @@ const SendingCredits = ({ setSendingCredits }) => {
         >
           Cancel
         </button>
-        <button className={stl.bottCta}>
+        <button className={stl.bottCta} onClick={handleCreditTransfer}>
           <IoSend />
         </button>
       </div>
