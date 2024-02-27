87dccae9d2ec9a5baa53e53fcc8f75f075e521d9
vandersarroman <vandersarroman@gmail.com>
2024-02-27 03:48:06 +0100

    Error styling handled

--- src/utils/userprofile/runecredits/SendingCredits/SendingCredits.jsx
+++ src/utils/userprofile/runecredits/SendingCredits/SendingCredits.jsx
@@ -1,12 +1,52 @@
 import stl from "./SendingCredits.module.css";
 import { IoSend } from "react-icons/io5";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import supabase from "../../../supabase/supabase";
+import { AuthContext } from "../../../authprovider/AuthProvider";
 
 const SendingCredits = ({ setSendingCredits }) => {
+  const { userID } = useContext(AuthContext);
   const [amountToSend, setAmountToSend] = useState("");
   const [receiverName, setReceiverName] = useState("");
+  const [error, setError] = useState("");
 
-  const handleCreditTransfer = () => {};
+  const handleCreditTransfer = async () => {
+    if (amountToSend === "" && receiverName === "") {
+      setError("Both");
+      return;
+    }
+    if (amountToSend === "") {
+      setError("No amount");
+      return;
+    }
+
+    try {
+      //Check if user exists before sending credits
+      const { data, error } = await supabase
+        .from("users_meta")
+        .select("username");
+      console.log(data);
+
+      if (error) {
+        throw new Error(error);
+      }
+
+      let recipientUserName = "";
+
+      data.forEach((addy) => {
+        if (addy.username.toLowerCase() === receiverName.toLowerCase()) {
+          recipientUserName = addy.username;
+        }
+      });
+
+      if (!recipientUserName) {
+        setError("No username");
+        return;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className={stl.sendingcredits}>
@@ -18,6 +58,10 @@ const SendingCredits = ({ setSendingCredits }) => {
           className={stl.inputStl}
           value={amountToSend}
           onChange={(e) => setAmountToSend(e.target.value)}
+          style={{
+            borderBottom:
+              error === "No amount" || error === "Both" ? "1px solid red" : "",
+          }}
         />
       </div>
       <div className={stl.sendWrap}>
@@ -27,7 +71,16 @@ const SendingCredits = ({ setSendingCredits }) => {
           className={stl.inputStl}
           placeholder="King Rom II"
           value={receiverName}
-          onChange={(e) => setReceiverName(e.target.value)}
+          onChange={(e) => {
+            setReceiverName(e.target.value);
+            setError("");
+          }}
+          style={{
+            borderBottom:
+              error === "No username" || error === "Both"
+                ? "1px solid red"
+                : "",
+          }}
         />
       </div>
       <div className={stl.sendWrap}>
