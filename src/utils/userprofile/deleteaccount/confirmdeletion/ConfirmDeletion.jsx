e1f770778536abe875c355399bdf8289590908a0
vandersarroman <vandersarroman@gmail.com>
2024-02-28 02:44:50 +0100

    Delete user endpoint created

--- src/utils/userprofile/deleteaccount/confirmdeletion/ConfirmDeletion.jsx
+++ src/utils/userprofile/deleteaccount/confirmdeletion/ConfirmDeletion.jsx
@@ -19,7 +19,21 @@ const ConfirmDeletion = () => {
   }, [confirmText, setApproved]);
 
   const handleAccountDeletion = async () => {
+    await fetch("https://netlify.app/.netlify/functions/server/deleteuser", {
+      method: "POST",
+      body: JSON.stringify({ userID: userID }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
     await supabase.from("users_meta").delete().eq("uid", userID);
+    await supabase.from("users").delete().eq("uid", userID);
+    await supabase.from("saved_inventories").delete().eq("uid", userID);
+    await supabase.from("saved_builds").delete().eq("uid", userID);
+    await supabase.from("runechat").delete().eq("uid", userID);
+    await supabase.from("poll_votes").delete().eq("uid", userID);
+    await supabase.from("item_favorites").delete().eq("uid", userID);
   };
 
   return (
