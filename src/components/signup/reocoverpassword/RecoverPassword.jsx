af5e43f4a68c5b125bc98cf8d11273a9f9deb7e2
vandersarroman <vandersarroman@gmail.com>
2024-02-28 22:32:34 +0100

    Border focus issue fixed

--- src/components/signup/reocoverpassword/RecoverPassword.jsx
+++ src/components/signup/reocoverpassword/RecoverPassword.jsx
@@ -1,17 +1,37 @@
 import HomeButton from "../../../utils/homebutton/HomeButton";
 import stl from "./RecoverPassword.module.css";
-
-////////////
-//Supabase//
-////////////
-
-/////////
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa6";
+import { useState } from "react";
 
 const RecoverPassword = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [eyeClicked, setEyeClicked] = useState(false);
+
+  const togglePasswordView = () => {
+    setShowPassword(!showPassword);
+    setEyeClicked(true);
+  };
+
   return (
-    <div className={stl.signup}>
+    <div className={stl.recover}>
       <HomeButton />
-      <div className={stl.modal}></div>
+      <div className={stl.modal}>
+        <span className={stl.setPassSpan}>Enter new password</span>
+        <div className={stl.inputWrapper}>
+          <input
+            type={showPassword ? "text" : "password"}
+            className={`${stl.passInput} ${eyeClicked ? stl.focusBorder : ""}`}
+            onClick={() => setEyeClicked(false)}
+          />
+          {showPassword && (
+            <FaEye className={stl.eye} onClick={togglePasswordView} />
+          )}
+          {!showPassword && (
+            <FaEyeSlash className={stl.eye} onClick={togglePasswordView} />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
