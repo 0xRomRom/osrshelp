fec639d166847a2167130df10793072914e41924
vandersarroman <vandersarroman@gmail.com>
2024-02-29 02:23:53 +0100

    Gearbuilder restyling

--- src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
+++ src/components/main/gearcalculator/prebuilds/PreBuilds.jsx
@@ -41,7 +41,6 @@ const PreBuilds = ({ handlePrebuildSetup, savedSlots, setSavedSlots }) => {
 
   return (
     <div className={stl.prebuilds}>
-      <h2 className={stl.hero}>Prebuilds</h2>
       <div className={`${stl.flipList} ${reversed ? stl.flipped : ""}`}>
         <div className={stl.freeBox}>
           <h3 className={stl.subHero}>Free</h3>
