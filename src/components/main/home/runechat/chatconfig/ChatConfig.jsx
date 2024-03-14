7cc603e5b1ffe7eb9b74139906751028a11b1cd8
vandersarroman <vandersarroman@gmail.com>
2024-03-15 00:44:31 +0100

    Issues fixed

--- src/components/main/home/runechat/chatconfig/ChatConfig.jsx
+++ src/components/main/home/runechat/chatconfig/ChatConfig.jsx
@@ -1,10 +1,16 @@
 import stl from "./ChatConfig.module.css";
 import ChatColor from "../../../../../utils/userprofile/ChatColor/ChatColor";
+import { AuthContext } from "../../../../../utils/authprovider/AuthProvider";
+import { useContext } from "react";
 
 const ChatConfig = () => {
+  const { storedColor } = useContext(AuthContext);
   return (
     <div className={stl.chatconfig}>
-      <ChatColor userColor={"#808080"} className={stl.chatColor} />
+      <ChatColor
+        userColor={storedColor || "#808080"}
+        className={stl.chatColor}
+      />
     </div>
   );
 };
--- src/utils/authprovider/AuthProvider.jsx
+++ src/utils/authprovider/AuthProvider.jsx
@@ -14,50 +14,46 @@ const AuthProvider = ({ children, setPlayerName }) => {
   const [ethereumDonateCount, setEthereumDonateCount] = useState(null);
   const [runeCredits, setRuneCredits] = useState(null);
 
-  const getPremium = useCallback(
-    async (uid) => {
-      try {
-        const { data } = await supabase
-          .from("users")
-          .select("*")
-          .eq("uid", uid)
-          .single();
+  const getPremium = useCallback(async (uid) => {
+    try {
+      const { data } = await supabase
+        .from("users")
+        .select("*")
+        .eq("uid", uid)
+        .single();
 
-        if (data) {
-          setEthereumDonateCount(data.etherdonate);
-          setLobsterDonateCount(data.lobsterdonate);
-          setRuneCredits(data.runecredits);
-        }
-        if (data?.premium) {
-          setPremiumUser(true);
-        } else {
-          setPremiumUser(false);
-        }
-      } catch (err) {
-        console.error(err);
+      if (data) {
+        setEthereumDonateCount(data.etherdonate);
+        setLobsterDonateCount(data.lobsterdonate);
+        setRuneCredits(data.runecredits);
       }
-
-      try {
-        const { data } = await supabase
-          .from("users_meta")
-          .select("username")
-          .eq("uid", uid)
-          .single();
-
-        if (data) {
-          // setPlayerName(data.username);
-          setStoredUsername(data.username);
-          // setStoredColor(data.usercolor);
-        }
-      } catch (err) {
-        console.error(err);
-        setPlayerName("");
-        setStoredUsername("");
-        setStoredColor("#808080");
+      if (data?.premium) {
+        setPremiumUser(true);
+      } else {
+        setPremiumUser(false);
       }
-    },
-    [setPlayerName]
-  );
+    } catch (err) {
+      console.error(err);
+    }
+
+    try {
+      const { data } = await supabase
+        .from("users_meta")
+        .select("*")
+        .eq("uid", uid)
+        .single();
+
+      if (data) {
+        setStoredUsername(data.username);
+        setStoredColor(data.usercolor);
+        console.log(data.usercolor);
+      }
+    } catch (err) {
+      console.error(err);
+      setStoredUsername("");
+      setStoredColor("#808080");
+    }
+  }, []);
 
   useEffect(() => {
     supabase.auth.onAuthStateChange(async (event, session) => {
